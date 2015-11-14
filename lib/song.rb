04badcdaf4faaf8e27816af88c7c7a7d13806183
github username <github email address>
2015-11-14 14:16:57 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   end
 
   def self.find_by_name(song_name)
-    self.all.detect { |song| song.name == song_name }
+    Song.all.detect { |song| song.name == song_name }
   end
 
   def self.find_or_create_by_name(song_name)
