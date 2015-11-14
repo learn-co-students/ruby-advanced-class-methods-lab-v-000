64914bcc1eda1c439b03563086663e0fb591e283
github username <github email address>
2015-11-14 14:56:38 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -29,11 +29,11 @@ class Song
   end
 
   def self.find_by_name(song_name)
-    Song.all.detect { |song| song.name == song_name }
+    self.all.detect { |song| song.name == song_name }
   end
 
   def self.find_or_create_by_name(song_name)
-    find_by_name(song_name) || create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
   end
 
   def self.alphabetical
@@ -41,26 +41,46 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    title = filename.split(" - ")
-    artist = title[0]
-    song_name, extension = title[1].split(".")
-
     song = self.new 
-    song.artist_name = artist 
-    song.name = song_name 
+    title = filename.gsub(/\w+[^-]\z/, "")
+    song.artist_name = title.scan(/\w+[^-]/)[0].strip
+    song.name = title.scan(/\w+[^-]+\w+[^.]/)[1]
     song 
   end
+
+  # Alternative self.new_from_filename solution 
+  #
+  # def self.new_from_filename(filename)
+  #   title = filename.split(" - ")
+  #   artist = title[0]
+  #   song_name, extension = title[1].split(".")
+
+  #   song = self.new 
+  #   song.artist_name = artist 
+  #   song.name = song_name 
+  #   song 
+  # end
 
   def self.create_from_filename(filename)
-    title = filename.split(" - ")
-    artist = title[0]
-    song_name, extension = title[1].split(".")
-
     song = self.create 
-    song.artist_name = artist 
-    song.name = song_name 
+    title = filename.gsub(/\w+[^-]\z/, "")
+    song.artist_name = title.scan(/\w+[^-]/)[0].strip
+    song.name = title.scan(/\w+[^-]+\w+[^.]/)[1]
     song 
   end
+
+  # Alternative self.create_from_filename solution
+  
+  # def self.create_from_filename(filename)
+  #   title = filename.split(" - ")
+  #   artist = title[0]
+  #   song_name, extension = title[1].split(".")
+
+  #   song = self.create 
+  #   song.artist_name = artist 
+  #   song.name = song_name 
+  #   song 
+  # end
 
   def self.destroy_all
     self.all.clear
