b611bbd287c9f5df9ce8686328bcf93dbcfdfd80
plai217 <philiplai217@gmail.com>
2015-11-14 21:20:28 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -13,59 +13,47 @@ class Song
   end
 
   def self.create
-    song = Song.new
+    song = self.new
     song.save
     song 
   end
 
   def self.new_by_name(name)
-    song = Song.new
+    song = self.new
     song.name = name
-    song
-  end
-
-  def self.new_by_artist(artist)
-    song = Song.new
-    song.artist_name = artist
     song
   end
 
   def self.create_by_name(name)
-    song = Song.new
+    song = self.create
     song.name = name
-    song.save
     song
   end
 
   def self.find_by_name(name)
-    Song.all.detect {|song| song.name == name}
+    self.all.detect {|song| song.name == name}
   end
 
   def self.find_or_create_by_name(name)
-    Song.find_by_name(name) || Song.create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
   def self.alphabetical
-    Song.all.sort_by{|song|song.name}
+    self.all.sort_by{|song|song.name}
   end
 
 
   def self.new_from_filename(file)
     title = file.sub(".mp3","").split(" - ").last
-    artist = file.sub(".mp3","").split(" - ").first
-    song = Song.new
+    artist = file.split(" - ").first
+    song = self.new
     song.name = title
     song.artist_name = artist
     song
   end
 
   def self.create_from_filename(file)
-    title = file.sub(".mp3","").split(" - ").last
-    artist = file.sub(".mp3","").split(" - ").first
-    song = Song.new
-    song.name = title
-    song.artist_name = artist
-    song.save
+    new_from_filename(file).save
   end
 
   def self.destroy_all
