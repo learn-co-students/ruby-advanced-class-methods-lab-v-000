f47293d2e8f9b16cd0aed278683b3d111d55ad62
Justin Thomas <jthomas0209@gmail.com>
2015-11-19 11:20:43 -0500

    Defined methods and variables for class Song

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,49 @@ class Song
     self.class.all << self
   end
 
+  def self.create()
+    song = Song.new
+    self.all << song
+    self.all.last
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    self.all << song
+    self.all.last
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.create_by_name(name) if self.find_by_name(name) == nil
+    self.find_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort{|x,y| x.name <=> y.name}
+  end
+
+  def self.new_from_filename(filename)
+    data = filename.split(" - ")
+    name = data[1][0..data[1].length-5]
+    artist = data[0]
+    song = self.create_by_name(name)
+    song.artist_name = artist
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    self.all << song
+    self.all.last
+  end
 end
