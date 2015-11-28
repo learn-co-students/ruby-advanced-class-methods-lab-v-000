cf51ed6a37b697fd5a6c88d91b6a3f8f7fe8cb54
babyshoes <dianecai@gmail.com>
2015-11-28 17:52:17 -0500

    all done

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,65 @@ class Song
     @@all
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    found = self.find_by_name(name)
+    while !found
+      self.create_by_name(name)
+      found = self.find_by_name(name)
+    end
+    found
+  end
+
+  def self.alphabetical
+    @@all.sort_by { |song| song.name }
+  end
+
+  def self.new_from_filename(filename)
+    filename = filename.split(' - ')
+    artist_name = filename[0]
+    song_name = filename[1].split('.')[0]
+    song = Song.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
   def save
     self.class.all << self
   end
+
+
 
 end
--- spec/song_class_methods_spec.rb
+++ spec/song_class_methods_spec.rb
@@ -52,7 +52,6 @@ describe "Song Class Methods" do
       song_1 = Song.create_by_name("Thriller")
       song_2 = Song.create_by_name("Blank Spaces")
       song_3 = Song.create_by_name("Call Me Maybe")
-
       expect(Song.alphabetical).to eq([song_2, song_3, song_1])
     end
   end
