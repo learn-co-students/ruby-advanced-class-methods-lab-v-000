d7b55453f693d9035322df415d309f8b5989910d
yoshikot <ytamaoki@gmail.com>
2015-12-12 17:40:32 -0500

    done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -12,14 +10,61 @@ class Song
     self.class.all << self
   end
 
-  def self.create(track)
+  def self.create
+    self.all << self.new
+    self.all[-1]
+  end
+
+  def self.new_by_name(track)
     song = self.new
     song.name = track
-    @@all << self.name
-    binding.pry
+    song
+  end
+
+  def self.create_by_name(track)
+    song = self.new_by_name(track)
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(track)
+    self.all.detect {|song| song.name == track}
+  end
+
+  def self.find_or_create_by_name(track)
+    if self.find_by_name(track) == nil
+      self.create_by_name(track)
+    else self.find_by_name(track)
+    end
+  end
+
+  def self.alphabetical
+    alphabetizable = []
+    self.all.collect do |song| 
+      alphabetizable << [song.name, song]
+    end
+    alphabetizable.sort.collect do |info|
+      info[1]
+    end
+  end
+
+  def self.new_from_filename(file)
+    artist = file[0..file.index(/ - /) - 1]
+    title = file[file.index(/ - /) + 3..file.index(/.mp3/) - 1]
+    song = self.new
+    song.name = title
+    song.artist_name = artist
+    song
+  end
+
+  def self.create_from_filename(file)
+    song = self.new_from_filename(file)
+    self.all << song
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
-
-song = Song.create("The Middle")
-puts Song.all.include?(song) #=> true
