02309f348edaeab21069469c1f3308604832686d
jbok4 <jbok4@aol.com>
2016-02-27 10:08:47 -0500

    setup methods

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,28 @@ class Song
     @@all
   end
 
+  def self.create(name)
+  end
+
+  def self.new_by_name(name)
+  end
+
+  def self.create_by_name(name)
+  end
+
+  def find_or_create_by_name(name)
+  end
+
+  def alphabetical
+    @@all.sort_by {|song| }
+  end
+
+  def self.new_from_filename(filename)
+  end
+
+  def self.destroy_all
+  end
+
   def save
     self.class.all << self
   end
