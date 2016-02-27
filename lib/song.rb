f8a79c76e1c01bd31504e9b308a920e79920d732
jbok4 <jbok4@aol.com>
2016-02-27 10:25:51 -0500

    2 passing tests

--- lib/song.rb
+++ lib/song.rb
@@ -6,13 +6,21 @@ class Song
     @@all
   end
 
-  def self.create(name)
+  def self.create(name, artist_name)
+    song = self.new
+    song.name = name
+    @@all << person
   end
 
   def self.new_by_name(name)
   end
 
   def self.create_by_name(name)
+
+  end
+
+  def self.find_by_name(name)
+    @@all.detect{|song| @@all.song == song}
   end
 
   def find_or_create_by_name(name)
@@ -26,6 +34,7 @@ class Song
   end
 
   def self.destroy_all
+    @@all.clear
   end
 
   def save
