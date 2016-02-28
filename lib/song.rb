24912829028447bf8fd100df25a8ef55c308b6a2
jbok4 <jbok4@aol.com>
2016-02-28 15:39:47 -0500

    a few passing so far

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,27 @@
 class Song
+
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
     @@all
   end
 
-  def self.create(name, artist_name)
+  def self.create
     song = self.new
-    song.name = name
-    @@all << person
+    Song.name = name
+    @@all << name
   end
 
   def self.new_by_name(name)
+    Song.name = name
   end
 
   def self.create_by_name(name)
-
+    #@@all << self.new
   end
+
 
   def self.find_by_name(name)
     @@all.detect{|song| @@all.song == song}
@@ -26,8 +30,9 @@ class Song
   def find_or_create_by_name(name)
   end
 
-  def alphabetical
-    @@all.sort_by {|song| }
+  def self.alphabetical
+    @@all.sort_by(&:downcase)
+    #also works: self.all.sort_by {|song| song.name}
   end
 
   def self.new_from_filename(filename)
