53320fa2ad862246466ea820c877a4f3bc99a065
LonghornRach <LonghornRach@users.noreply.github.com>
2016-03-07 10:52:51 -0600

    Done

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,65 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+  	song = self.new
+  	song.save
+  	song
+  end
+require 'pry'
+  def self.new_by_name(name)
+  	song = self.new
+  	song.name = name
+  	song
+  end
+
+  def self.create_by_name(name)
+  	song = self.new
+  	song.name = name
+  	song.save
+  	song
+  end
+
+  def self.find_by_name(name)
+  	Song.all.detect { |song| song.name==name }
+  end
+
+  def self.find_or_create_by_name(name)
+  	@@all.include?(name) ? Song.find_by_name(name) : Song.create_by_name(name)
+  end
+
+  def self.alphabetical
+  	output = []
+  	@@all.each { |song| output << song.name }
+  	output.sort.collect { |name| Song.find_by_name(name)  }
+  end
+
+  def self.new_from_filename(file)
+  	file.chomp!(".mp3")
+  	split = file.split(" - ")
+  	artist_name, name = split[0], split[1]
+  	#binding.pry
+  	song = self.new
+  	song.artist_name = artist_name
+  	song.name = name  	
+  	song
+  end
+
+  def self.create_from_filename(file)
+  	file.chomp!(".mp3")
+  	split = file.split(" - ")
+  	artist_name, name = split[0], split[1]
+  	song = self.new
+  	song.artist_name = artist_name
+  	song.name = name
+  	song.save
+  	song
+  end
+
+  def self.destroy_all
+  	@@all.clear
+  end
+
 end
+
+#Song.new_from_filename("Thundercat - For Love I Come.mp3")
