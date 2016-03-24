5768afc5e1c6ad03007f5a17952c4464b685b5a1
jchu4483 <jchu4483@gmail.com>
2016-03-24 16:01:00 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,24 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def self.all
+  def self.all #=>Song.all
     @@all
   end
 
   def save
     self.class.all << self
+    #self #Song Class #all instances of Song << self
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new(name)
+
   end
 
 end
