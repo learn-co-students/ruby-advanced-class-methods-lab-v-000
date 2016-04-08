7796fb235b5c37e920518dc1fc50f7a2816ff880
Christian Whitesides <whitesidescr@gmail.com>
2016-04-08 16:46:40 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
   # Custom constructor - #create
   def self.create
     @song = self.new
-    self.all << @song
+    @song.save
     @song #returning the song
   end
 
@@ -29,39 +29,49 @@ class Song
   end
 
   def self.find_by_name(name)
-    self.all.detect { |song| song.name == name }
+    self.all.detect { |s| s.name == name }
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == nil
-      self.create_by_name(name)
-    else
-      self.find_by_name(name)
-    end
+    # if self.find_by_name(name) == nil
+    #   self.create_by_name(name)
+    # else
+    #   self.find_by_name(name)
+    # end
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
   def self.alphabetical
-    self.all.sort_by { |song| song.name }
+    self.all.sort_by { |s| s.name }
   end
 
   def self.new_from_filename(filename)
-    filename_array = filename.split(" - ")
-    data = filename_array.collect do |element|
-      element.chomp(".mp3")
-    end
+    # filename_array = filename.split(" - ")
+    # data = filename_array.collect do |element|
+    #   element.chomp(".mp3")
+    # end
+    parts = filename.split(" - ")
 
-    artist_name = data[0]
-    name = data[1]
+    artist_name = parts[0]
+    name = parts[1].gsub(".mp3", "")
 
     song = self.new
     song.artist_name = artist_name
     song.name = name
-
     song
   end
 
   def self.create_from_filename(filename)
-    (self.new_from_filename(filename)).save
+    # (self.new_from_filename(filename)).save
+
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
   end
 
   def self.destroy_all
