44ce01354238e8720f8f86dc1fb05f681aec920e
Lush Sleutsky <lushsleutsky@gmail.com>
2016-04-18 20:32:00 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -39,8 +39,8 @@ class Song
   end
 
   def self.alphabetical
-    self.all.sort_by do |a|
-      a.name
+    self.all.sort_by do |song|
+      song.name
     end
   end
 
@@ -56,18 +56,11 @@ class Song
   end
 
   def self.create_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].gsub(".mp3", "")
-
-    song = self.create
-    song.name = song_name
-    song.artist_name = artist_name
-    song
+    song = self.new_from_filename(filename)
+    song.save
   end
 
   def self.destroy_all
     self.all.clear
   end
-
 end
