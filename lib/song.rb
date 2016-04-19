eb1e28580ea41a528088c967c7441167e47391e5
DownsideupmediaUSA <brandondecarlo@live.com>
2016-04-19 09:20:37 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -52,16 +52,6 @@ class Song
     song
   end
 
-  def self.new_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].gsub(".mp3" , "")
-
-    song = self.new
-    song.name = song_name
-    song.artist_name = artist_name
-    song
-  end
 
   def self.create_from_filename(filename)
     parts = filename.split(" - ")
