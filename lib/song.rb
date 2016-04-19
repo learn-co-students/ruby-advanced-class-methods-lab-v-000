3161f405550c99169f1a7b13843e12f51fa3a782
Drew Pham <drewvpham@gmail.com>
2016-04-19 16:00:18 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,62 +6,67 @@ class Song
         end
 
         def save
-    self.class.all << self
+
+  self.class.all << self
   end
 
   def self.create
-          song = self.new
-    song.save
-    song
+song=self.new
+song.save
+song
   end
 
-        def self.new_by_name(song_name)
-                song=self.new
-                song.name=song_name
-                song
-        end
+  def self.new_by_name(song_name)
+                  song=self.new
+                  song.name=song_name
+                  song
+          end
+
 
         def self.create_by_name(song_name)
-                song=self.create
-                song.name=song_name
-                song
+song=self.create
+song.name=song_name
+song
 
         end
 
         def self.find_by_name(song_name)
-                self.all.detect do |s| s.name==song_name
-                end
+self.all.detect do |d| d.name==song_name
+end
+
 
         end
 def self.find_or_create_by_name(song_name)
-    self.find_by_name(song_name) || self.create_by_name(song_name)
+self.find_by_name(song_name) || self.create_by_name(song_name)
   end
 
         def self.alphabetical
-                 self.all.sort_by{|s| s.name}
+self.all.sort_by do |s|
+  s.name
+end
   end
-        
+
 
   def self.new_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].gsub(".mp3", "")
-
+    part = filename.split(" - ")
+    song_name = part[1].gsub(".mp3", '')
+    artist_name = part[0]
     song = self.new
     song.name = song_name
     song.artist_name = artist_name
-    song
+song
+
   end
 
   def self.create_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].gsub(".mp3", "")
-
+    part = filename.split(" - ")
+    song_name = part[1].gsub(".mp3", '')
+    artist_name = part[0]
     song = self.create
     song.name = song_name
     song.artist_name = artist_name
-    song
+  song
+
   end
 
   def self.destroy_all
@@ -70,7 +75,3 @@ def self.find_or_create_by_name(song_name)
 
 
 end
-
-
-
-
