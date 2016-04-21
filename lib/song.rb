58d5090a5b6c4e5e4ce82cba4af3f3f3c36cfa2f
Michael Randell <michael.james.randell@gmail.com>
2016-04-21 17:47:10 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,19 @@ class Song
     @@all
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+
+
+  def self.new_by_name(song)
+
+    #self.all.detect{|s| s == song}
+  end
+
   def save
     self.class.all << self
   end
