6502563c69e63302f745525f207c5e176a7d1926
johnfelixespinosa <johnfelix.espinosa@icloud.com>
2016-04-28 17:23:38 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+ 
   def self.all
     @@all
   end
@@ -11,39 +11,40 @@ class Song
   end
 
   def self.create
-    song = Song.new
-    song.save
-    song
+  	song = Song.new
+  	song.save
+  	song
   end
 
   def self.new_by_name(song_name)
-    song = self.new
-    song.name = song_name
-    song
+  	song = self.new
+  	song.name = song_name
+  	song
   end
 
   def self.create_by_name(song_name)
-    song = self.create
-    song.name = song_name
-    song
+  	song = self.create
+  	song.name = song_name
+  	song
   end
 
   def self.find_by_name(song_name)
-    self.all.detect{|s| s.name == song_name}
+  	self.all.detect {|song|  song.name == song_name}
   end
 
   def self.find_or_create_by_name(song_name)
-    self.find_by_name(song_name) || self.create_by_name(song_name)
+    self.create || self.find_by_name
   end
 
   def self.alphabetical
-    self.all.sort_by{|s| s.name}
+  	self.all.sort_by {|song| song.name }
   end
 
-  def self.new_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].gsub(".mp3", "")
+  def self.new_from_filename(song_file)
+    song_file.slice!(".mp3")
+    split_file = song_file.split(" - ")
+    song_name = split_file[1]
+    artist_name = split_file[0]
 
     song = self.new
     song.name = song_name
@@ -51,19 +52,29 @@ class Song
     song
   end
 
-  def self.create_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].gsub(".mp3", "")
+  def self.create_from_filename(song_file)
+  	song_file.slice!(".mp3")
+  	split_file = song_file.split(" - ")
+    song_name = split_file[1]
+    artist_name = split_file[0]
 
     song = self.create
     song.name = song_name
     song.artist_name = artist_name
-    song
   end
 
   def self.destroy_all
-    self.all.clear
+  	self.all.clear
   end
 
 end
+
+
+
+
+
+
+
+
+
+
