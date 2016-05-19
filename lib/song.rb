a1e5614fb5ecaab9f908a34cb75fce1c2f1d3db5
rnhutsko <rnhutsko@gmail.com>
2016-05-19 10:58:22 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,27 @@ class Song
     self.class.all << self
   end
 
+  def initialize (name)
+    @name = name
+    self.class.all << self
+  end
+
+
+  def self.create
+    song = self.new(name)
+  end
+
+  def self.new_by_name(name)
+    self.new(name)
+  end
+
+  def self.create_by_name (name)
+    @name = self.new(name)
+  end
+
+  def self.find_by_name(name)
+    idx = self.all.index(name)
+    self.all[idx]
+  end
+
 end
