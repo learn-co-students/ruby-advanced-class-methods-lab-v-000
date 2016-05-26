2e8315807fe1d9a32cc9f0118ff76c18d217177d
tpstar <hanlee12@gmail.com>
2016-05-26 00:20:17 -0400

    code adv class methods lab

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,58 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    new_song = self.new
+    self.all << new_song
+    new_song
+  end
+
+  def self.new_by_name(name)
+    new_song = self.new
+    new_song.name = name
+    new_song
+  end
+
+  def self.create_by_name(name)
+    new_song = self.new
+    new_song.name = name
+    self.all << new_song
+    new_song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if !find_by_name(name)
+        create_by_name(name)
+    else
+        find_by_name(name)
+     end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(mp3)
+    new_song = self.new
+    new_song.artist_name = mp3.split(" - ").shift
+    new_song.name = mp3.split(" - ").pop.split(".").shift
+    new_song
+  end
+
+  def self.create_from_filename(mp3)
+    new_song = self.new
+    new_song.artist_name = mp3.split(" - ").shift
+    new_song.name = mp3.split(" - ").pop.split(".").shift
+    self.all << new_song
+    new_song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
