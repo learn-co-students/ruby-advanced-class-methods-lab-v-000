58c8dbf440896a265df8d9d0c97ab19a5e9e0ddb
Bob Habig <rfhab2@gmail.com>
2016-06-14 22:41:14 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,50 +10,51 @@ class Song
     self.class.all << self
   end
 
-  def self.create
+  def self.create #class constructor
     song = self.new
     song.save
     song
   end
 
-  def self.new_by_name(name)
+  def self.new_by_name(name) #class constructor
     song = self.new
     song.name = name
     song
   end
 
-  def self.create_by_name(name)
+  def self.create_by_name(name) #class constructor
     song = self.create
     song.name = name
     song
   end
 
   def self.find_by_name(name)
-    self.all.detect{|song| song.name == name}
+    self.all.detect {|song| song.name == name}
   end
 
   def self.find_or_create_by_name(name)
-    self.create_by_name(name) unless self.find_by_name(name) == true
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
   end
 
   def self.alphabetical
-    self.all.sort_by{|s| s.name}
+    self.all.sort_by {|song| song.name}
   end
 
-  def self.new_from_filename(file_name)
-    info = file_name.split(" - ")
+  def self.new_from_filename(filename)
+    info = filename.split(" - ")
     artist_name = info[0]
     song_name = info[1].gsub(/\a\s|\.\w+/, "")
+
     song = self.create
     song.artist_name = artist_name
     song.name = song_name
     song
   end
 
-  def self.create_from_filename(file_name)
-    self.new_from_filename(file_name).save
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).save
   end
-
+  
   def self.destroy_all
     self.all.clear
   end
