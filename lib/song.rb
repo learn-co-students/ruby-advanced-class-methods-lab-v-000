6dd253a30e825e33d82d68b155b75fd317f7ddf2
Keith Wellman <keith.wellman@icloud.com>
2016-06-27 16:04:55 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,59 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name }
+  end
+
+  def self.new_from_filename(filename)
+    song = self.new
+    filename_arr = filename.split(" - ")
+    song.artist_name = filename_arr[0]
+    song.name = filename_arr[1].chomp(".mp3")
+    song
+  end
+
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).save
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 
   def self.all
     @@all
