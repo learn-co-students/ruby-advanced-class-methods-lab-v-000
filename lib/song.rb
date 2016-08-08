a7e9890ae33c82d52b617e4e00e0c870ddd2439f
Certain125 <amanita.certain@gmail.com>
2016-08-08 16:06:24 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,69 +1,61 @@
-require 'pry'
+
 
 class Song
   attr_accessor :name, :artist_name
-  @@all = []
+  @@all = [ ]
+
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
 
   def self.all
     @@all
   end
 
-  def save
-    self.class.all << self
-  end
-
-  def self.create
-    song = self.new
-    song.save
-    song
-  end
-
   def self.new_by_name(name)
-    song = self.new #if i do song = self.create this interferes w/ the alphabetical method b/c the .create method
-                    #will save each song and the tests will say that i have two of each song in my song array
+    song = self.new
     song.name = name
     song
   end
 
   def self.create_by_name(name)
-    song = self.new_by_name(name)
-    song.save
+    song = self.new
+    song.name = name
+    @@all << song
     song
   end
 
   def self.find_by_name(name)
-    self.all.detect do |single|
-      single.name == name
-    end
+    self.all.detect {|s| s.name == name}
   end
 
   def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create_by_name(name) #remember to use argument when defining & calling methods
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
   def self.alphabetical
-    self.all.sort_by do |single|
-      single.name
-    end
+    self.all.sort_by {|x| x.name}
   end
 
-  def self.new_from_filename(file_ID)
+  def self.new_from_filename(filename)
     song = self.new
-    filename = file_ID.split(" - ")
-    name = filename[1]
-    artist_name = filename[0]
-    song.name = name.sub(".mp3", "") #find alternative to delete, sub and gsub
+    artist_name = filename.split(" - ")[0]
     song.artist_name = artist_name
+    name = filename.split(" - ")[1]
+    song.name = name.sub(".mp3", "")
     song
   end
 
-  def self.create_from_filename(file_ID)
-    song = self.new_from_filename(file_ID)
-    song.save
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    @@all << song
   end
 
   def self.destroy_all
-    self.all.clear
+    @@all.clear
   end
+
 
 end
