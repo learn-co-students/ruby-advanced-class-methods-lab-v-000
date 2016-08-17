1bf778d3ebd4b13a5afa9737e55b6bb1f9a41c8b
Kenlyn Terai <kkterai@gmail.com>
2016-08-17 19:14:55 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -53,12 +53,12 @@ class Song
   end
 
   def self.create_from_filename(filename)
-    song = self.create
     segments = filename.split("-")
-    artist_name = filename[0]
-    s_name= filename[1].gsub(".mp3", "")
+    artist_name = segments[0]
+    s_name= segments[1].gsub(".mp3", "")
+    song = self.create
+    song.name = s_name
     song.artist_name = artist_name
-    song.name= s_name
     song
   end
 
