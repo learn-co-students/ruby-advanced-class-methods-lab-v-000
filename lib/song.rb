0ce91b1dc543b8fe1fbadb41e0d956f8a4b132f2
Kenlyn Terai <kkterai@gmail.com>
2016-08-17 03:13:36 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -13,14 +13,38 @@ class Song
   end
 
   def self.create
-    @song = self.new
-    self.all << @song
-    @song
+    song = self.new
+    self.all << song
+    song
   end
 
   def self.new_by_name(name)
-    @song = self.new
+    song = self.new
     song.name =name
-    @song
+    song
   end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name =name
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.all.include?(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by{ |name| name }
+  end
+
 end
