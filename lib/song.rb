25d95229a50f7bcac7ad5e39148415e5f124298d
Kenlyn Terai <kkterai@gmail.com>
2016-08-17 02:36:05 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,15 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    @song = self.new
+    self.all << @song
+    @song
+  end
+
+  def self.new_by_name(name)
+    @song = self.new
+    song.name =name
+    @song
+  end
 end
