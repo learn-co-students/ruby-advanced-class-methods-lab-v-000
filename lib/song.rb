79282765cdeab2fcbb838e2417de082f591e37f1
Bob Habig <rfhab2@gmail.com>
2016-08-20 18:55:12 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Song
-  attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artist_name, :genre
 
   @@all = []
 
@@ -26,16 +26,39 @@ class Song
     @@all
   end
 
-  def self.find_by_name(name) #.detect would be more concise, but this adds flexibility for multiple songs with same name
-    self.all.select{|song| song.name == name}.each{|song| return song}
+  def self.find_by_name(name)  #.detect would be more concise, but this adds flexibility for multiple songs with same name
+    all.detect{|song| song.name == name}
   end
 
   def self.find_or_create_by_name(name)
-    if all.include?(name)
-      find_by_name(name)
-    else
-      create_by_name(name)
-    end
-    #create_by_name(name) unless find_by_name(name) != nil
+    !find_by_name(name).nil? ? find_by_name(name) : create_by_name(name)
+  end
+
+  def self.alphabetical
+    all.sort_by!{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    song = filename.split(" - ")
+    artist = song[0]
+    song_name = song[1].gsub(/[.].*/,"")
+
+    @song = self.new_by_name(song_name)
+    @song.artist_name = artist
+    @song
+  end
+
+  def self.create_from_filename(filename)
+    song = filename.split(" - ")
+    artist = song[0]
+    song_name = song[1].gsub(/[.].*/,"")
+
+    @song = self.create_by_name(song_name)
+    @song.artist_name = artist
+    @song
+  end
+
+  def self.destroy_all
+    all.clear
   end
 end
