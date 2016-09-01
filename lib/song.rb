3759b0aa51b3d7b3e1d3d7ca3528c82f4ac86759
Clare <clare.e.horton@gmail.com>
2016-09-01 21:26:05 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   end
   
   def self.find_by_name(name)
-    @@all.detect{|song| song.name == name}
+    self.all.detect{|song| song.name == name}
   end
   
   def self.find_or_create_by_name(name)
