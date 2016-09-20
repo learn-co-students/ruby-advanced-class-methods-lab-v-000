416eec7b62da7b8f3e2d1cdf9ca5b12113aacde4
Emmanuel Owusu <e_owusu@live.com>
2016-09-20 21:58:45 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,11 @@
 class Song
-  attr_accessor :name, :artist_name, :song
+  attr_accessor :name, :artist_name
   @@all = []
+
+  def initialize
+    #closed for modification
+  end
+
 
   def self.all
     @@all
@@ -10,8 +15,80 @@ class Song
     self.class.all << self
   end
 
-  def self.create(song)
-    @@all << self.new
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song.save
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.detect do |song|
+      song.name == song_name
+    end
+  end
+
+  def self.find_or_create_by_name(song_name)
+    song_array = nil
+    if self.find_by_name(song_name) == false
+      return song_array
+    else
+      self.create_by_name(song_name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song_name| song_name.name}
+  end
+
+  def self.new_from_filename(file_in_mp3_format)
+    mp3_file = file_in_mp3_format.split("-")
+    song_in_mp3_format = mp3_file[1]
+    song_file = song_in_mp3_format.split(".")
+    name = song_file[0].strip
+    artist_name = mp3_file[0].strip
+
+    song = self.new
+
+    song.name = name
+    song.artist_name = artist_name
+
+    song
+  end
+
+  def self.create_from_filename(file_in_mp3_format)
+    mp3_file = file_in_mp3_format.split("-")
+    song_in_mp3_format = mp3_file[1]
+    song_file = song_in_mp3_format.split(".")
+    name = song_file[0].strip
+    artist_name = mp3_file[0].strip
+
+    song = self.new
+
+    song.name = name
+    song.artist_name = artist_name
+
+    song
+
+    @@all << song
+  end
+
+
+  def self.destroy_all
+    @@all.clear
   end
 
 end
