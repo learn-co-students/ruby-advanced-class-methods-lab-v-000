679ec461e1b69e855df623e7f21431325df45bba
Neil Ryan <neilmarkoliverryan@gmail.com>
2016-10-26 16:04:06 -0700

    Update song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -29,29 +29,29 @@ class Song
   end
 
   def self.find_by_name(name_string)
-  	@@all.find {|song| song.name == name_string}
+    @@all.find {|song| song.name == name_string}
   end
 
   def self.find_or_create_by_name(name_string)
     if
-  	  self.find_by_name(name_string) == nil
-  	  song = self.create_by_name(name_string)
-  	  song
-  	else
-  	  self.find_by_name(name_string)
-  	end
+      self.find_by_name(name_string) == nil
+        song = self.create_by_name(name_string)
+  	song
+    else
+      self.find_by_name(name_string)
+    end
   end
 
   def self.alphabetical
-  	@@all.sort! { |a,b| a.name <=> b.name }
+    @@all.sort! { |a,b| a.name <=> b.name }
   end
 
   def self.new_from_filename(artist_song_string)
-  	song = self.new
-  	data = artist_song_string.split(" - ")
-  	song.artist_name = data[0]
-	  song_type = data[1].split(".")
-	  song.name = song_type[0]
+    song = self.new
+    data = artist_song_string.split(" - ")
+    song.artist_name = data[0]
+    song_type = data[1].split(".")
+    song.name = song_type[0]
     song
   end
 
