db2bf59040c54192bb1974e8e4e2e376bc2f051e
Mary Umoh <umohm12@gmail.com>
2016-11-08 22:49:15 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,71 @@ class Song
     self.class.all << self
   end
 
+ def self.create
+   song = self.new
+   @@all << song
+   song
+ end
+
+ def self.new_by_name(name)
+   song = self.new
+   song.name = name
+   song
+ end
+
+ def self.create_by_name(name)
+   song = self.new
+   song.name = name
+   @@all << song
+   song
+ end
+
+def self.find_by_name(name)
+  @@all.detect {|song| song.name == name}
+end
+
+def self.find_or_create_by_name(name)
+  if !!self.find_by_name(name)
+            self.find_by_name(name)
+        else
+          self.create_by_name(name)
+        end
+    end
+
+
+def self.alphabetical
+  @@all.sort_by {|song|  song.name}
+end
+
+def self.new_from_filename(mp3)
+    mp3.chomp!(".mp3")
+    data = mp3.split(" - ")
+    artist_name = data[0]
+    name = data[1]
+    # Make a new instance
+    song = self.new
+    song.artist_name = artist_name
+    song.name = name
+    # Return the song
+    song
+end
+
+def self.create_from_filename(mp3)
+  mp3.chomp!(".mp3")
+   data = mp3.split(" - ")
+   artist_name = data[0]
+   name = data[1]
+   # Make a new instance
+   song = self.new
+   song.artist_name = artist_name
+   song.name = name
+   # Return the song
+   @@all << song
+ end
+
+
+def self.destroy_all
+  @@all.clear
+end
+
 end
