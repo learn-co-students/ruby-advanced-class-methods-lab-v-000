3dc78332f3ac3a7b75dffbe64dcda21a5c1dc87e
Tina Kane Netherton <tina.kane@gmail.com>
2016-11-19 23:21:13 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -14,6 +14,34 @@ class Song
     song
   end
 
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name)
+    song = self.new
+  end
+
+  def self.alphabetical
+    self.all.sort_by! { |song| song.name }
+  end
+
+  def self.new_from_filename
+
   def save
     self.class.all << self
   end
