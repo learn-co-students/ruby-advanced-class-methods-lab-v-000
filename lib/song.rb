b8b3b852e0da135a43c0159cd5a97ab64701071e
Tyson Philips <sangdencre@gmail.com>
2016-11-23 19:16:27 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,71 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.detect{|song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by do |x|
+      x.name
+    end
+  end
+
+  def self.new_from_filename(filename)
+    data = filename.sub(/\.[^.]+\z/, '').split(' - ')
+    artist = data[0]
+    song_name = data[1]
+
+    song = self.new
+    song.artist_name = artist
+    song.name = song_name
+    song
+  end
+
+
+  def self.create_from_filename(filename)
+  	data = filename.sub(/\.[^.]+\z/, '').split(' - ')
+    artist = data[0]
+    song_name = data[1]
+
+    song = self.new
+    song.artist_name = artist
+    song.name = song_name
+    self.all << song
+    song
+
+
+  end
+
+  def self.destroy_all
+  	self.all.clear
+  end
+
 end
+
+
+song1 = Song.create_from_filename("Taylor Swift - Blank Space.mp3")
+puts song1.inspect
+puts Song.all.inspect
