9e7fd81f764a0fd285a677628b8bfed9d100f628
Scott Wakeman <swakeman@ca.rr.com>
2016-12-05 22:49:22 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,32 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def inialize(name, artist_name)
+    @name=name
+    @artist_name=artist_name
+    @@all << self
+    song.create
+    song.new_by_name
+
+  end
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = Song.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(song)
+    song = Song.new
+    song.name
+
   end
 
 end
