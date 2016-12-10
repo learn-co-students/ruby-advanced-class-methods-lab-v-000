2d8daffbd0857655fbd1bac998560bab41742c92
Scott Wakeman <swakeman@ca.rr.com>
2016-12-10 21:47:35 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -32,14 +32,12 @@ class Song
   end
 
   def self.find_by_name(song_name)
-    self.all.detect{|songs| songs.name == song_name}
+    self.all.detect{|song| song.name == song_name}
   end
 
   def self.find_or_create_by_name(song_name) #accept a string name for a song
-    if self.all.detect{|songs| !(songs.name == song_name)}
-      song = Song.new     #create a new song with the name
-    else #self.create_by_name(song_name) #return a matching song instance with that name
-      song_name #return the song instance.
-    end
+    self.find_by_name(song_name)
+    self.create_by_name(song_name)
+
   end
 end
