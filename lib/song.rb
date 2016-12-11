f791ca9712471506ced50955d67d77defb7ad32d
Yamini Kumawat <yaminikumawat94@gmail.com>
2016-12-11 17:54:58 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,63 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.each do |w|
+      if w.name == name
+        return w
+      end
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    if @@all.include?(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort {|a, b| a.name <=> b.name}
+  end
+
+  def self.new_from_filename(file)
+    song = Song.new
+    array = file.split(" - ")
+    song.artist_name = array[0]
+    song.name = array[1].split(".")[0]
+    song
+  end
+
+  def self.create_from_filename(file)
+    song = Song.new
+    array = file.split(" - ")
+    song.artist_name = array[0]
+    song.name = array[1].split(".")[0]
+    @@all << song
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
