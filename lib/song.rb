cc2c1424abd581a9a722900e5b84971da504c65d
Terry-Thompson <terry.lyn.thompson@gmail.com>
2017-01-13 10:00:30 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,39 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(name=nil, artist_name=nil)
+    @name = name
+    @artist_name = artist_name 
+  end # of initialize
+
   def self.all
     @@all
-  end
+  end # of self.all
+
 
   def save
     self.class.all << self
-  end
+  end # of save 
 
-end
+
+  def self.create
+    song = self.new
+    song.name = name
+    song.save
+    song 
+  end # of self.create
+
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song 
+  end # of self.new_by_name
+
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save 
+  end # of self.create_by_name
+
+end # of class 
