f70ceb734b8f3b56813e23d0f30db3c3b519e496
charlie lee <ctlee9096@gmail.com>
2017-02-08 03:56:46 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,21 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
   def self.all
     @@all
   end
-
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
   def save
     self.class.all << self
   end
-
+  def self.create(name)
+    self.all.push(name)
+    binding.pry
+    self.all
+    binding.pry
+  end
 end
