6bcc0871d53b0ca8785967124f410c1f2c99027c
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-02-13 13:46:40 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,64 @@ class Song
     @@all
   end
 
+  def initialize
+
+  end
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.new_by_name(name)
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.each do |song|
+      if song.name == name
+        return song
+      end
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    if song == []
+      song = self.create_by_name(name)
+    end
+    song
+  end
+
+  def self.alphabetical
+    @@all.sort_by do |song|
+      song.name
+    end
+  end
+
+  def self.new_from_filename(filename)
+    filename = filename.split(' - ')
+    song_name = filename[1].split('.')
+    song = self.find_or_create_by_name(song_name[0])
+    song.artist_name = filename[0]
+    song
+  end
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+  end
+  def self.destroy_all
+    @@all.clear
+  end
+
   def save
     self.class.all << self
   end
