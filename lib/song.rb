40c6912fb4167b88541d67cdd7af7dc681c587b1
bentonwong <bentonwong@gmail.com>
2017-02-17 22:00:06 -0800

    my solution

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,7 @@
 class Song
+
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
@@ -8,6 +10,72 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    song.name = ""
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if @@all.detect{|song| song.name == name} == true
+      song
+    else
+      song = self.new
+      song.name = name
+      @@all << song
+      song
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    split_up_file = filename.chomp('.mp3').split(" - ")
+    artist_name = split_up_file[0]
+    name = split_up_file[1]
+
+    song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    split_up_file = filename.chomp('.mp3').split(" - ")
+    artist_name = split_up_file[0]
+    name = split_up_file[1]
+
+    song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    @@all << song
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
