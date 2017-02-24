67f84615d0253f68c3a63dae8bf285b09bcfb5c2
Scott Atkins <satkins16@gmail.com>
2017-02-24 05:07:02 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,64 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    self.all << song = Song.new
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = Song.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    self.all << song = Song.new
+    song.name = song_name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.detect {|song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name) == true
+    else
+      create_by_name(song_name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|x| x.name}
+  end
+
+  def self.new_from_filename(filename)
+    filename.slice! ".mp3"
+    rows = filename.split(" - ")
+    rows.collect do |row|
+      @song_name = rows[1]
+      @artist = rows[0]
+    end
+    new_song = Song.new_by_name(@song_name)
+    new_song.artist_name = @artist
+    new_song
+  end
+
+  def self.create_from_filename(filename)
+    filename.slice! ".mp3"
+    rows = filename.split(" - ")
+    rows.collect do |row|
+      @song_name = rows[1]
+      @artist = rows[0]
+    end
+    new_song = Song.create_by_name(@song_name)
+    new_song.artist_name = @artist
+    new_song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
