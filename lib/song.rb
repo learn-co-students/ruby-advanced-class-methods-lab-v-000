c72fe18f7c7aaa2a48e99aedc9210274be36c5d2
sam oteo <tortillabar@me.com>
2017-03-14 22:58:40 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,18 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  def self.create
+    song = self.new
+
+    binding.pry
+  end
+
+  def save
+    self.class.all << self
+  end
 
   def self.all
     @@all
