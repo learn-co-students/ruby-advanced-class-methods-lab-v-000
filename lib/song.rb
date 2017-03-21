0280fa439d68f00e2480d2913a0e010b802dd015
thesilentc <thesilentc@me.com>
2017-03-20 21:46:37 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,57 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    @@all.detect{|title| title.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+# combine the two newly created methods with  'OR' operator -> ||
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+
+  def self.alphabetical
+       self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    result = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song
+  end
+
+    def self.destroy_all
+      self.all.clear
+    end
+
 end
