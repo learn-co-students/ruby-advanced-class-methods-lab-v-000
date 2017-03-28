34926363aae3c86f45eac818bc9f8f3cd669a9b1
sam oteo <tortillabar@me.com>
2017-03-28 17:32:29 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,12 +6,53 @@ class Song
 
   def self.create
     song = self.new
-
-    binding.pry
+    song.save
+    song
   end
 
-  def save
-    self.class.all << self
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.detect { |song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.all.include?(name)
+      self.find_by_name(name)
+    else self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by! { |song| song.name.downcase}
+    @@all
+  end
+
+  def self.new_from_filename(filename)
+    data = filename.split(" - ").collect { |x| x.gsub(/.mp3/, '')}
+    song = self.new_by_name(data[1])
+    song.artist_name = data[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
   def self.all
