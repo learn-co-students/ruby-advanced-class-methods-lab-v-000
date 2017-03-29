4e3fbe4f3c23dbcbaade8ede7ed49b85f7a2c5fc
William Barela <william.barela.dev@gmail.com>
2017-03-29 06:38:38 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -33,8 +33,7 @@ class Song
   end
 
   def self.find_or_create_by_name(song_name)
-    song_exists = self.all.any?{|song| song.name == song_name}
-    song_exists ? self.find_by_name(song_name) : self.create_by_name(song_name)
+    self.find_by_name(song_name) ? self.find_by_name(song_name) : self.create_by_name(song_name)
   end
 
   def self.alphabetical
