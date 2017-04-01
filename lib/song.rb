4b9b29a29cc3a8238dc3b649379a4ae171296d16
Daniela Carvajal <d.carvajal.agama@gmail.com>
2017-04-01 04:08:53 +0000

    Done ruby advanced methods lab

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,57 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by!{ |song| song.name }
+  end
+
+  def self.new_from_filename(filename)
+    song = filename.split("-")
+    song_new = Song.new
+    song_new.name = song[1].strip.chomp(".mp3")
+    song_new.artist_name = song[0].strip
+    song_new
+  end
+
+  def self.create_from_filename(filename)
+    song = filename.split("-")
+    song_new = Song.new
+    song_new.name = song[1].strip.chomp(".mp3")
+    song_new.artist_name = song[0].strip
+    song_new
+    @@all << song_new
+    song
+  end
+
+ def self.destroy_all
+   @@all.clear
+ end
+
 end
