f1d15d890ba4c278d5b962de1b437bc3bcfe9af5
Elias Ventura <eliasventura23@gmail.com>
2017-05-10 19:47:33 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+ 
   def self.all
     @@all
   end
-
+ 
   def save
     self.class.all << self
   end
@@ -17,25 +17,24 @@ class Song
   end
 
   def self.new_by_name(name)
-    song= self.new
-    song.name= name
+    song= Song.new
+    song.name = name
+    song.save
     song
   end
 
   def self.create_by_name(name)
-    song = self.create
+    song= Song.create
     song.name= name
     song
-  end
+ end
 
-  def self.find_by_name(name)
-    self.all.detect do |song|
-      song.name== name
-    end
-  end
+ def self.find_by_name(name)
+   self.all.detect {|s|s.name == name}
+ end
 
-  def self.find_or_create_by_name(name)
-    find_by_name(name) || create_by_name(name)
+ def self.find_or_create_by_name(name)
+   find_by_name(name) || create_by_name(name)
   end
 
   def self.alphabetical
@@ -43,28 +42,24 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    parts= filename.split(" - ")
-    song_name = parts[1].gsub(".mp3", "")
-    artist_name= parts[0]
+    parts = filename.split(" - ")
 
-    song= self.new
-    song.name= song_name
-    song.artist_name= artist_name
+    song= Song.new
+    song.name= parts[1].gsub(".mp3", "")
+    song.artist_name = parts[0]
     song
   end
 
-    def self.create_from_filename(filename)
-      parts= filename.split(" - ")
-      song_name = parts[1].gsub(".mp3", "")
-      artist_name= parts[0]
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
 
-      song= self.create
-      song.name= song_name
-      song.artist_name= artist_name
-      song
-    end
-
-    def self.destroy_all
-      self.all.clear
-    end
+    song= Song.create
+    song.name= parts[1].gsub(".mp3", "")
+    song.artist_name = parts[0]
+    song
   end
+
+  def self.destroy_all
+    self.all.clear
+  end
+end
