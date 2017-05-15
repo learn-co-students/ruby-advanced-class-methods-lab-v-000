cd1e4166a87ac90f5d3fa0273e2f75730bbefa9a
Danielle Croxton <danikrist@gmail.com>
2017-05-15 18:49:37 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,27 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    
+  end
+
 end
