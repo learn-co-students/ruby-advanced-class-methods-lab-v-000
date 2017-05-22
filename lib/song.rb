7dd9b6a25c96b8f8a0b0456553897ad2e99d7728
Noah Summers <summers.noah@gmail.com>
2017-05-22 16:06:15 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,56 @@ class Song
     @@all
   end
 
+  def self.create
+    created = self.new
+    created.save
+    created
+  end
+
+  def self.new_by_name(input)
+    named = self.new
+    named.name = input
+    named
+  end
+
+  def self.create_by_name(input)
+    created_named = self.new
+    created_named.name = input
+    created_named.save
+    created_named
+  end
+
+  def self.find_by_name(input)
+    @@all.detect {|song| song.name == input}
+  end
+
+  def self.find_or_create_by_name(input)
+    find_by_name(input) || create_by_name(input)        
+  end
+
+  def self.alphabetical
+    @@all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(input)
+    from_filename = self.new
+    filename_parse = input.match(/(.*)\s\u{002d}\s(.*)\p{Punct}/)
+    from_filename.name = filename_parse[2]
+    from_filename.artist_name = filename_parse[1]
+    from_filename
+  end
+
+  def self.create_from_filename(input)
+    created_filename = new_from_filename(input)
+    created_filename.save
+    created_filename
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+  
   def save
     self.class.all << self
   end
-
 end
