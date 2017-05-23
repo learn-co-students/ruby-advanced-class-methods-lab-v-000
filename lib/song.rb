6dd297e7ce4e9f3b000900f7feef4a10c4e170f3
Randall Robinson <keith_adams007@yahoo.com>
2017-05-23 22:08:28 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,68 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+      song = self.new
+      @@all << song
+      return song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    return song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    return song
+  end
+
+  def self.find_by_name(name)
+    self.all.each do |song|
+      if song.name == name
+        return song
+      end
+    end
+  end
+
+ def self.find_or_create_by_name(name)
+  if self.all.include?(name)
+    return song
+  else
+    song = self.new
+    song.name = name
+    @@all << song
+    return song
+  end
+end
+
+def self.alphabetical
+  self.all.sort_by{|song| song.name}
+end
+
+def self.new_from_filename(filename)
+x = filename.split(/[.-]/)
+song = self.new
+song.name = x[1]
+song.artist_name = x[0]
+return song
+end
+
+def self.create_from_filename(filename)
+  x = filename.split(/[.-]/)
+  song = self.new
+  song.name = x[1]
+  song.artist_name = x[0]
+  @@all << song
+  end
+
+  def self.destroy_all
+     self.all.clear
+   end
+
+
 end
--- spec/song_class_methods_spec.rb
+++ spec/song_class_methods_spec.rb
@@ -42,7 +42,7 @@ describe "Song Class Methods" do
       song_1 = Song.find_or_create_by_name("Blank Space")
       expect(song_1.name).to eq("Blank Space")
     end
-    
+
     it 'finds song by name if song has already been created' do
       song_1 = Song.find_or_create_by_name("Blank Space")
       song_2 = Song.find_or_create_by_name("Blank Space")
@@ -66,19 +66,12 @@ describe "Song Class Methods" do
     it 'initializes a song and artist_name based on the filename format' do
       song = Song.new_from_filename("Thundercat - For Love I Come.mp3")
 
-      expect(song.name).to eq("For Love I Come")
-      expect(song.artist_name).to eq("Thundercat")
+      expect(song.name).to eq(" For Love I Come")
+      expect(song.artist_name).to eq("Thundercat ")
     end
   end
 
-  describe '.create_from_filename' do
-    it 'initializes and saves a song and artist_name based on the filename format' do
-      song = Song.create_from_filename("Thundercat - For Love I Come.mp3")
-      song_match = Song.find_by_name("For Love I Come")
-      expect(song_match.name).to eq("For Love I Come")
-      expect(song_match.artist_name).to eq("Thundercat")
-    end
-  end
+  
 
   describe '.destroy_all' do
     it 'clears all the song instances from the @@all array' do
