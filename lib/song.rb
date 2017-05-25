85e3aeade3b3a9e9779b4ecdd497e5a66acb52c9
gb23 <gregbenj@hotmail.com>
2017-05-25 14:02:56 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,13 +9,15 @@ class Song
   end
 
   def self.new_by_name(name)
-    song=self.create
+    song=self.new
     song.name = name
     song
   end
 
   def self.create_by_name(name)
-    self.new_by_name(name)
+    song = self.create
+    song.name = name
+    song
   end
 
   def self.find_by_name(name)
