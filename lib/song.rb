9afe2b768524eb1c6b1ed051f4d5150e709b3361
Luisa Scavo <luisascavo@gmail.com>
2017-05-31 00:29:01 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,60 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.create
+    song.name = name
+    song
+  end
+
+  def self.new_from_filename(filename)
+    song = Song.new
+    song.filename_split(filename)
+  end
+
+  def self.create_from_filename(filename)
+    song = Song.create
+    song.filename_split(filename)
+  end
+
+  def filename_split(filename)
+    puts "I made it to the method"
+    info = filename.split(" - ")
+    @artist_name = info[0]
+    @name = info[1].chomp(".mp3")
+    self
+  end
+
   def self.all
     @@all
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
   def save
