c149d81706ad2b0250698015ee1ef11e75c8bf29
kyleblee <hello.kylelee@gmail.com>
2017-06-13 15:41:17 -0400

    lab complete

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,54 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    new_song = Song.new
+    new_song.save
+    new_song
+  end
+
+  def self.new_by_name(name)
+    new_song = Song.new
+    new_song.name = name
+    new_song
+  end
+
+  def self.create_by_name(name)
+    new_song = Song.new_by_name(name)
+    new_song.save
+    new_song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(name)
+    name_split = name.split(" - ")
+    artist_name = name_split[0]
+    song_name = name_split[1].scan(/./).slice(0, name_split[1].length-4).join
+    new_song = Song.new_by_name(song_name)
+    new_song.artist_name = artist_name
+    new_song
+  end
+
+  def self.create_from_filename(name)
+    new_song = Song.new_from_filename(name)
+    new_song.save
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
