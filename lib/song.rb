5f14a9842886c61cf647a91b7fafdf40746c62eb
Seb Gertz <sebgertz@gmail.com>
2017-06-19 22:16:41 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,36 @@ class Song
     self.class.all << self
   end
 
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def self.create(name)
+    song = self.new
+    save    
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    self.all << name
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.all.detect{|song| song.name == name}
+      song
+    else
+      self.create_by_name
+    end
+  end
 end
