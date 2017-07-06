bee939730dbb56dd1aa0fbe06368b84152bdb0d2
Kevin Powell <kevdpow@gmail.com>
2017-07-06 23:22:26 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,33 @@ class Song
   end
 
   def self.alphabetical
-    self.sort_by{|a| a.name.downcase}
+    self.all.sort_by{|a| a.name.downcase}
+  end
+
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = Song.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song.save
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
