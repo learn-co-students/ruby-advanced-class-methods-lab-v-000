88510b523e39e60e68394a2db1c8fb98fbb42776
churchwerks <steven.lennis@icloud.com>
2017-08-02 16:40:22 -0500

    five tests passing

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +11,29 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new_by_name(song_name)
+    song
+  end
+
+  def self.find_by_name(song_name)
+    #self.all.detect(self.create_by_name(song_name)) {|item| item.name == song_name}
+    self.all.detect {|item| item.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
 end
