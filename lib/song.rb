866bab037bcf5ab79621e5b79b2a4f22b817a144
Joanna Sese <jmgsese@gmail.com>
2017-08-02 22:19:43 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
 
   def self.find_or_create_by_name(name)
     # if self.find_by_name(name)
-    #   return self.find_by_name(name)
+    #   self.find_by_name(name)
     # else
     #   self.create_by_name(name)
     # end
