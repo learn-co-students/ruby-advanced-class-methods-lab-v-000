3f4b28b4556ecba3fba36b259c4cedc8a1f3df9d
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-08 17:20:11 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,70 @@ class Song
     @@all
   end
 
+  def initialize(name = nil)
+    @name = name
+    self.class.all << self
+  end
+
   def save
     self.class.all << self
   end
+
+  def self.create
+    song = Song.new
+    song.save
+    song
+    #@@all << self
+  end
+
+  def self.find_by_name(name)
+    @@all.detect{ |song| song.name == name}
+    #if @@all.include?(name)
+    #  name
+    #else
+    #  puts "Name is not found in array."
+    #end
+  end
+
+  def self.find_or_create_by_name(name)
+    ##if @@all.include?(name)
+      self.find_by_name(name) || self.create_by_name(name)
+    ##end
+  end
+
+  def self.new_by_name(name)
+    self.new(name)
+  end
+
+  def self.create_by_name(name)
+    self.new(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by{ |song| song.name }
+  end
+
+  def self.new_from_filename(file_name)
+    split_array_no_whitespace = []
+    split_array = file_name.split(/[-\.]/)
+    split_array.each do |value|
+      split_array_no_whitespace << value.split(" ")
+    end
+
+    song = self.new
+    song.name = split_array_no_whitespace[1].join(" ")
+    song.artist_name = split_array_no_whitespace[0].join(" ")
+    @@all << self
+    song
+  end
+
+  def self.create_from_filename(file_name)
+    new_from_filename(file_name)
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
 
 end
