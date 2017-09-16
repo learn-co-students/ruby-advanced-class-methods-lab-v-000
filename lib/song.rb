c83f0bf7eb52dbb3060387fe5a93ef14e3471297
Sergio Correa <correa.sergio@gmail.com>
2017-09-16 20:12:50 +0000

    up to .find_or_create_by_name

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,47 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = Song.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect { |song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    binding.pry
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by{ |song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+
+  end
+
+  def create_from_filename
+  end
+
+  def self.destroy_all
   end
 
 end
