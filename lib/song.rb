03e7d403aa150ca2b8e12ea51ead97f6b438d1e2
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-09-20 03:14:27 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,17 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    @@all << self.new
+    @@all[-1]
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
 end
+
+#Song.new_by_name("Fuckker")
