8134955c790c6c0038bb1d916bd9974ef6d5d1a2
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-09-20 17:29:46 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -18,11 +18,54 @@ class Song
   end
 
   def self.new_by_name(song_name)
-    song = self.new
-    song.name = song_name
-    song
+    new_song = self.new
+    new_song.name = song_name
+    new_song
+  end
+
+  def self.create_by_name(song_name)
+    new_song = self.new
+    @@all << new_song
+    new_song.name = song_name
+    new_song
+  end
+
+  def self.find_by_name(song_name)
+    @@all.find {|index| index.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name)
+      self.find_by_name(song_name)
+    else
+      self.create_by_name(song_name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|index| index.name}
+  end
+
+  def self.new_from_filename(file_name)
+    new_song = self.new
+    file_array = file_name.split(" - ")
+    new_song.name = (file_array[1][0..-5])
+    new_song.artist_name = file_array[0]
+    new_song
+  end
+
+  def self.create_from_filename(file_name)
+    new_song = self.new
+    file_array = file_name.split(" - ")
+    new_song.name = (file_array[1][0..-5])
+    new_song.artist_name = file_array[0]
+    self.all << new_song
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
 
-#Song.new_by_name("Fuckker")
+#song = Song.new_from_filename("Thundercat - For Love I Come.mp3")
