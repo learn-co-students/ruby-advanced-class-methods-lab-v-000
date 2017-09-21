12ad1b780806d53ba8e78d26f612acd101259aba
Sophie Chang <soapycee@icloud.com>
2017-09-21 19:56:10 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,58 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.create
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find { |song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    did = find_by_name(name)
+    if did == nil
+    create_by_name(name)
+    else
+      did
+    end
+  end
+
+  def self.alphabetical
+    @@all.uniq.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(file)
+    song = Song.new
+    song_array = file.split(" - ")
+    song_name = song_array[1].chomp(".mp3")
+    song.name = song_name
+    song.artist_name = song_array[0]
+    song
+  end
+
+  def self.create_from_filename(file)
+    song = self.new_from_filename(file)
+    song.save
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
 end
