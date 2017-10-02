5126b1dfa4d40174cdd0d4112365141c651e7e7e
Rony Boeck <ramg.9@live.com>
2017-10-02 21:07:06 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,63 @@ class Song
     @@all
   end
 
+  def self.create
+    newsong = self.new
+    @@all << newsong
+    newsong
+  end
+
+  def self.new_by_name(name)
+    newsong = self.new
+    newsong.name = name
+    newsong
+  end
+
+  def self.create_by_name(name)
+    newsong = self.new
+    @@all << newsong
+    newsong.name = name
+    newsong
+  end
+
+  def self.find_by_name(name)
+    @@all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    newname = filename.split(/\s\-\s|\./)
+    song = self.new
+    song.artist_name = newname[0]
+    song.name = newname[1]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    newname = filename.split(/\s\-\s|\./)
+    song = self.new
+    song.artist_name = newname[0]
+    song.name = newname[1]
+    song.save
+    song
+  end
+
   def save
     self.class.all << self
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
