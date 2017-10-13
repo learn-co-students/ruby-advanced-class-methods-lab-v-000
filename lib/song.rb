f48c97caab16c00010071e56c5779d182eee449c
John Mooney <jrmint46@gmail.com>
2017-10-13 21:12:07 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,66 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.detect do |x|
+      x.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      return self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by! do |x|
+      x.name
+    end
+    @@all
+  end
+
+  def self.new_from_filename(name)
+    song = self.new
+    namesplit = name.split(" - ")
+    song.artist_name = namesplit[0]
+    song.name = namesplit[1].chomp(".mp3")
+    song
+  end
+
+  def self.create_from_filename(name)
+    song = self.new
+    namesplit = name.split(" - ")
+    song.artist_name = namesplit[0]
+    song.name = namesplit[1].chomp(".mp3")
+    @@all << song
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
+
+#binding.pry
