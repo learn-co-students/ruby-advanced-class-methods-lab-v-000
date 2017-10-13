b80a63e12e37358ae364919bd9f5bc52ae05c328
Amber Blahnik <blissedtodays@gmail.com>
2017-10-13 16:56:23 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -53,15 +53,8 @@ class Song
     song
   end
 
-  def self.create_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].gsub(".mp3", "")
-
-    song = self.create
-    song.name = song_name
-    song.artist_name = artist_name
-    song
+  def self.create_from_filename(name)
+    self.new_from_filename(name).save
   end
 
   def self.destroy_all
