58c3e0e8d3a848559ab13b928a31f1be09ddf26b
Michael Hirschberger <mlhirschberger@gmail.com>
2017-10-16 03:21:35 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -33,11 +33,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == nil
-      self.create_by_name(name)
-    else
-      self.find_by_name(name)
-    end
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
   def self.alphabetical
