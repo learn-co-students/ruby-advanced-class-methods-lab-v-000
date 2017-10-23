15ab7e1bed239c4039e4d5cd0e7f0a5f43734332
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-23 00:42:11 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,46 @@ class Song
   end
 
   def save
-    self.class.all << self
+    @@all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end
 end
+
+def Song.find_or_create_by_name(name)
+  self.find_by_name(name) || self.create_by_name(name)
+end
+
+def Song.alphabetical
+  self.all.sort_by {|song| song.name}
+end
+
+def Song.new_from_filename(song)
+  @song = song
+  @artist_name = artist_name
+end
+
+def Song.destroy_all
+  self.all.clear
+end 
