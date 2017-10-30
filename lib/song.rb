90aa328b466593bdf2fa42554cc05ca704bec49f
Christopher DiStasio <github email address>
2017-10-30 17:31:22 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -18,8 +18,17 @@ class Song
     song
   end
 
-  def self.new_by_name
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
 
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
   end
 
 end
