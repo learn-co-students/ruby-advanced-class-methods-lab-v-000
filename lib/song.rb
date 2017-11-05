8fbfd5d3b85692b03842d11a87b8df9117775845
Miwa Hirano <paintfx@gmail.com>
2017-11-05 04:35:08 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,63 @@
 class Song
+
   attr_accessor :name, :artist_name
+
   @@all = []
 
-  def self.all
-    @@all
-  end
 
-  def save
-    self.class.all << self
-  end
+    def self.all
+      @@all
+    end
+
+    def save
+      self.class.all << self
+    end
+
+    def self.create
+      new_song = self.new
+      @@all << new_song
+      new_song
+    end
+
+    def self.new_by_name(song_name)
+      new_song = self.new
+      new_song.name = song_name
+      new_song
+    end
+
+    def self.create_by_name(song_name)
+      new_song = self.new
+      new_song.name = song_name
+      @@all << new_song
+      new_song
+    end
+
+    def self.find_by_name(song_name)
+      self.all.detect{|song| song.name == song_name}
+    end
+
+    def self.find_or_create_by_name(song_name)
+      self.find_by_name(song_name) || self.create_by_name(song_name)
+    end
+
+    def self.alphabetical
+      self.all.sort_by {|song| song.name}
+    end
+
+    def self.new_from_filename(filename)
+      song = self.new
+      song.name = filename.split(" - ")[1].chomp(".mp3")
+      song.artist_name = filename.split(" - ")[0]
+      song
+    end
+
+    def self.create_from_filename(filename)
+      new_song = self.new_from_filename(filename)
+      @@all << new_song
+    end
+
+    def self.destroy_all
+      self.all.clear
+    end
 
 end
