298e71d36e92f5b380bd6d9cbd07252013bbebe8
Yosa Yon <yosa.yon@gmail.com>
2017-11-10 03:12:02 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,73 +1,60 @@
 class Song
   attr_accessor :name, :artist_name
-  @@all = []
+  @@all = [] #class variable to hold all songs
 
-  def self.all
+  def self.all #class getter method
     @@all
   end
 
-  def save
+  def save #instance method to save itself into the class variable @@all
     self.class.all << self
   end
 
-def self.create
-  song = self.new
-  @@all << song
-  song
-end
-
-def self.new_by_name(name)
-  song = self.new
-  song.name = name
-  song
-end
-
-def self.create_by_name(name)
-  song = self.new
-  song.name = name
-  @@all << song
-  song
-end
-
-def self.find_by_name(name)
-  self.all.detect{|song|song.name == name}
-end
-
-def self.find_or_create_by_name(name)
-  if self.find_by_name(name) != nil
-    self.find_by_name(name)
-  else
-    create_by_name(name)
+  def self.create #class method to custom construct a new song and save itself
+    song = self.new
+    song.save
+    song
   end
-end
 
-def self.alphabetical
-  self.all.sort_by{|song| song.name}
-end
+  def self.new_by_name(name) #class method to custom construct a new song and give it the property name
+    song = self.new
+    song.name = name
+    song
+  end
 
-def self.new_from_filename(filename)
-  data = filename.slice(0,filename.length-4).split(" - ")
-  name = data[1]
-  artist_name = data[0]
+  def self.create_by_name(name) #class method to custom construct, save, create name property, and return song
+    song = self.create
+    song.name = name
+    song
+  end
 
-  song = self.new
-  song.name = name
-  song.artist_name = artist_name
-  song
-end
+  def self.find_by_name(name)#class method to detect and return the song if the song exists in the class variable.
+    self.all.detect{|song|song.name == name}
+  end
 
-def self.create_from_filename(filename)
-  data = filename.slice(0,filename.length-4).split(" - ")
-  name = data[1]
-  artist_name = data[0]
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
 
-  song = self.create
-  song.name = name
-  song.artist_name = artist_name
-  song
-end
+  def self.alphabetical
+    self.all.sort_by{|song|song.name}
+  end
 
-def self.destroy_all
-  self.all.clear
-end
+  def self.new_from_filename(filename)
+    data = filename.slice(0,filename.length-4).split(" - ")
+    artist_name = data[0]
+    song_name = data[1]
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
