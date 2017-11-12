55777f776fb922f25f42ea44d72ee3bd97e65a79
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2017-11-12 15:12:20 -0500

    Implemented spequired custom class constructors, class finders, and class operators

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,53 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect do |song|
+      name == song.name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ||
+    self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    attributes= filename.split(' - ')
+    song = self.create_by_name(attributes[1].split('.')[0])
+    song.artist_name = attributes[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    self.all << song
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
