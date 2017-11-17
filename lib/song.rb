0c71b8c29095a7d5631b2fe79eeedb61a4f018d5
charityyoungblood <youngblood.charity@gmail.com>
2017-11-16 16:35:13 -0800

    added comments on how to structure code, added create class method

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +11,21 @@ class Song
   def save
     self.class.all << self
   end
+  
+  # create the following class methods - include the "self" keyword in the method name:
+#    - create 
+  def self.create 
+    # Song.new - instantiate
+    self.new 
+    binding.pry
+  end 
+#    - new_by_name
+#    - create_by_name 
+#    - find_by_name 
+#    - find_or_create_by_name 
+#    - alphabetical 
+#    - new_from_filename
+#    - create_from_filename
+#    - destroy_all 
 
 end
