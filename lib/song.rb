af998f8249ffefe060dd7f2030e65f3c4008cfc5
Sharmina Akhtar <sakht318@gmail.com>
2017-11-22 10:07:11 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,26 @@ class Song
     self.class.all << self
   end
 
+  def Song.create
+    @@all >> Song.new
+  end
+
+  def Song.new_by_name(name)
+    songs = self.new
+    songs.name = name
+    @@all >> name
+  end
+
+  def Song.create_by_name
+    Song.all.each{|songs| puts "#{songs.name}"}
+  end
+
+  def Song.find_by_name(name)
+    self.all.detect{|person| person.name == name}  
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
