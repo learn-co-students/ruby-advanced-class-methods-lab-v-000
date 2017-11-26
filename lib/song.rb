f7db3e5f7ea3dbb9c2621f692b7809b3c9ce8399
Sharmina Akhtar <sakht318@gmail.com>
2017-11-26 17:46:56 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,21 +11,73 @@ class Song
   end
 
   def Song.create
-    @@all >> Song.new
+    #initializes a song and saves it to the @@all class variable either literally
+    #or through class method Song.all(self.all)
+    #should return song instance initialized and saved
+    song = self.new
+    song.save
+    song
   end
 
-  def Song.new_by_name(name)
-    songs = self.new
-    songs.name = name
-    @@all >> name
+  def Song.new_by_name(new_name)
+    #takes in the string name of a song and returns a song instance with that
+    #name set as its name property and the song being saved into the `@@all` class variable.
+    song = self.new
+    song.name = new_name
+    song
   end
 
-  def Song.create_by_name
-    Song.all.each{|songs| puts "#{songs.name}"}
+
+  def Song.create_by_name (create_name)
+    #takes in the string name of a song and returns a song instance with that
+    #name set as its name property and the song being saved into the `@@all` class variable.
+    song = self.new
+    song.name = create_name
+    song.save
+    song
+
   end
 
-  def Song.find_by_name(name)
-    self.all.detect{|person| person.name == name}  
+  def Song.find_by_name(find_name)
+    #accepts the string name of a song and returns the matching instance of
+    #the song with that name
+    self.all.detect do |x|
+      x.name == find_name
+    end
+  end
+
+  def Song.find_or_create_by_name(name)
+    #accept a string name for a song and either return a matching song instance
+    #with that name or create a new song with the name and return the song instance.
+
+      if Song.find_by_name(name) == nil
+        Song.create_by_name(name)
+      else
+        Song.find_by_name(name)
+      end
+
+
+  end
+
+  def Song.alphabetical
+    #returns all the songs in ascending (a-z) alphabetical order
+    self.all.sort_by!{ |x| x.name.downcase }
+  end
+
+  def Song.new_from_filename(file)
+    mp3 = self.new
+    mp3.name = file.split(/[^a-zA-Z\s]|\s-\s/)[1]
+    mp3.artist_name = file.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    mp3
+
+  end
+
+  def Song.create_from_filename(file)
+    mp3 = self.new
+    mp3.name = file.split(/[^a-zA-Z\s]|\s-\s/)[1]
+    mp3.artist_name = file.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    mp3.save
+    mp3
   end
 
   def self.destroy_all
