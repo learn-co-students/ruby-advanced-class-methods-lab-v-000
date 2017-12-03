954cffa22fb1fd653bc6fedd75c309fccd219508
JOSELY CASTRO <joselycas@gmail.com>
2017-12-03 17:25:13 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,10 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def initialize
-    self
-    @@all << self
-  end
+  #def initialize
+  #  self
+  #  @@all << self
+#  end
 
   def self.all
     @@all
