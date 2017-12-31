6231e72408799f4169fdd2899a0d55d9fb98c10d
Ahmad <github email address>
2017-12-31 14:56:22 -0800

    advanced methods

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,54 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+  	i = self.new
+  	i.save
+  	i
+  end
+
+  def self.new_by_name(name)
+  	new_song = self.new
+  	new_song.name = name
+  	new_song
+  end
+
+  def self.create_by_name(name)
+  	i = self.new
+  	i.name = name
+  	i.save
+  	i
+  end
+
+  def self.find_by_name(name)
+  	@@all.detect{|s| s.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+  	self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+  	self.all.sort_by {|s| s.name}
+  end
+
+  def self.new_from_filename(file_name)
+  	data = file_name.split(" - ").each { |x| x.chomp!(".mp3")}
+  	 artist_name = data[0]
+  	 name = data[1]
+  	 i = self.new
+  	 i.artist_name = artist_name
+  	 i.name = name
+  	 i
+  end
+
+  def self.create_from_filename(file_name)
+  	self.new_from_filename(file_name).save
+  end
+
+  def self.destroy_all
+  	@@all.clear
+  end
+
+
 end
