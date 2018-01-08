c2692b598c52be7a0482c9feb017ee494628b0d0
Judy McGoldrick <jjeanius@verizon.net>
2018-01-08 03:54:37 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ end
 def self.create_from_filename(file_name)
   new_song = self.new
   new_song.save
-  new_song.name = file_name.split(' - ')[1]. split(".")[0]
+  new_song.name = file_name.split(' - ')[1].split(".")[0]
   new_song.artist_name = file_name.split(" - ")[0]
   @@all << new_song
 end
