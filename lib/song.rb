2fd26b64576719328c1e2b7bd963660ceb8101c4
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-11 19:14:29 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +11,42 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+      song = self.new
+      self.all << song
+      song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name  ## don't just use the variable. call the method to expose the variable
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name  ## don't just use the variable. call the method to expose the variable
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(string_name)
+    self.all.detect {|song| song.name == string_name}
+  end
+
+  def self.find_or_create_by_name(title)
+    if find_by_name(title)
+      found_song = find_by_name(title)
+    else
+      create_by_name(title)
+    end
+  end
+
+  def self.alphabetical
+    binding.pry
+    self.all.sort
+  end
+
 
 end
