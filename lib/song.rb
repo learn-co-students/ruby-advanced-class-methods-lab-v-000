9491a6969234fc6131792eaf633a7b25644e6539
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-12 15:34:18 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -44,9 +44,33 @@ class Song
   end
 
   def self.alphabetical
-    binding.pry
-    self.all.sort
+    self.all.sort_by {|x| x.name}
   end
+
+  def self.new_from_filename(file_name)
+    #accept one argument
+    #seperate string into array by "-"
+    #assign array[0] to artist_name
+    #remove ".mp3" from array[1]
+    #assign array[1] to song_name
+
+    #create a new instance of the song class with .new
+    #assign name with name writer (song.name = name)
+    #assign song with artist_name writer
+    #return song
+    #end
+    file_array = file_name.split("-")
+    artist_name = file_array[0].rstrip
+
+    song_array = file_array[1].split(".")
+    name = song_array[0].lstrip
+
+    song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    song
+  end
+
 
 
 end
