ece08fd51f15dfed85398256ac230316a8c98246
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-12 17:08:17 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,8 @@
 require 'pry'
 
+
+#remember to call previous methods instead of re-writing similar code. for example
+#instead of calling self.new then pushing into array, just call the create method (which includes save)
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -14,7 +17,7 @@ class Song
 
   def self.create
       song = self.new
-      self.all << song
+      song.save ## or self.all << song
       song
   end
 
@@ -25,9 +28,8 @@ class Song
   end
 
   def self.create_by_name(song_name)
-    song = self.new
+    song = self.create #create creates the song and saves it ## or self.new then save
     song.name = song_name  ## don't just use the variable. call the method to expose the variable
-    self.all << song
     song
   end
 
@@ -43,11 +45,18 @@ class Song
     end
   end
 
+=begin
+the above code could also be written
+self.find_by_name(title) || self.create_by_name(title)
+
+
+=end
+
   def self.alphabetical
     self.all.sort_by {|x| x.name}
   end
 
-  def self.new_from_filename(file_name)
+  def self.new_from_filename(file_name)  #this function makes new song but doesnt save(create)
     #accept one argument
     #seperate string into array by "-"
     #assign array[0] to artist_name
@@ -59,13 +68,20 @@ class Song
     #assign song with artist_name writer
     #return song
     #end
-    file_array = file_name.split("-")
-    artist_name = file_array[0].rstrip
+    file_parts = file_name.split(" - ")
+    artist_name = file_parts[0]
+    #rstrip takes last space off of front
 
-    song_array = file_array[1].split(".")
+    song_array = file_parts[1].split(".")
     name = song_array[0].lstrip
+    #lstip returns a copy of string with leading whitespace removed.
+    #" Thunderstruck" => "Thunderstruck"
+    #the above code could be simplified with gsub, which takes all occurances
+    #of a pattern substituted for the second argument.
+    #  song_name = parts[1].gsub(".mp3", "")
 
-    song = self.new
+
+    song = self.new  #not self.create here since we don't want to save
     song.name = name
     song.artist_name = artist_name
     song
@@ -84,16 +100,14 @@ class Song
     #save song
     #return song
     #end
-    file_array = file_name.split("-")
-    artist_name = file_array[0].rstrip
+    file_parts = file_name.split(" - ")
+    artist_name = file_parts[0]
 
-    song_array = file_array[1].split(".")
-    name = song_array[0].lstrip
+    song_name = file_parts[1].gsub(".mp3", "")
 
-    song = self.new
-    song.name = name
+    song = self.create
+    song.name = song_name
     song.artist_name = artist_name
-    self.all << song
     song
   end
 
