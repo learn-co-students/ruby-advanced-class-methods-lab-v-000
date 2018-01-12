b70ba3b7d2ef23bedff6815bf21e68fe760da71f
Andrew Hong <hongandrew16@gmail.com>
2018-01-12 00:53:17 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -6,59 +7,64 @@ class Song
     @@all
   end
 
+  def save
+    self.class.all << self
+  end
+
   def self.create
     song = Song.new
     song.save
     song
   end
 
-  def save
-    self.class.all << self
-  end
-
-  def self.new_by_name(name)
+  def self.new_by_name(song_name)
     song = self.new
-    song.name = name
+    song.name = song_name
     song
   end
 
-  def self.create_by_name(name)
+  def self.create_by_name(song_name)
     song = self.create
-    song.name = name
+    song.name = song_name
     song
   end
 
-  def self.find_by_name(name)
-    self.all.find { |song| song.name == name}
+  def self.find_by_name(song_name)
+    self.all.detect{|s| s.name == song_name}
   end
 
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create_by_name(name)
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
   end
 
   def self.alphabetical
-    self.all.sort_by {|song| song.name}
+    self.all.sort_by{|s| s.name}
   end
 
   def self.new_from_filename(filename)
-    split_name = filename.split(/( - |\.)/)
-    song = self.new_by_name(split_name[2])
-    song.artist_name = split_name[0]
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
     song
   end
 
   def self.create_from_filename(filename)
-    split_name = filename.split(/( - |\.)/)
-    song = self.create_by_name(split_name[2])
-    song.artist_name = split_name[0]
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
     song
   end
 
   def self.destroy_all
-    @@all.clear
+    self.all.clear
   end
-
-
-
 
 end
