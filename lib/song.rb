210c578847eef2ab7cc5855e302284a4fe8017d2
kanndide <kanndide@gmail.com>
2018-01-14 18:24:23 +0300

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -75,19 +75,3 @@ class Song
 
 end
 
-# def self.new_from_csv(csv_data)
-#    rows = csv_data.split("\n")
-#    people = rows.collect do |row|
-#      data = row.split(", ")
-#      name = data[0]
-#      age = data[1]
-#      company = data[2]
-# 
-#      person = self.new # This is an important line.
-#      person.name = name
-#      person.age = age
-#      person.company = company
-#      person
-#    end
-#    people
-#  end
