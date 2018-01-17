eec9cae66408d233105429efdb1150e64831ddec
Ingrid Wong <yw340@georgetown.edu>
2018-01-17 18:03:41 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +11,88 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+    # class constructor
+    # initializes a song and saves it to @@all
+      # through class method @Song.all
+    # return the song instance
+
+    self.all << self.new
+    self.all.last
+  end
+
+  def self.new_by_name(name)
+    #class constructor
+    # input: string name of a song: name
+    # output: a song instance with that name set as its name
+
+    song = self.new
+    song.name = name
+    song
+
+  end
+
+  def self.create_by_name(name)
+    #class constructor
+    # input: string name
+    # output: song instance, song saved to @@all
+
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    #class finder
+    # input: string name
+    # output: matching instance
+    self.all.each do |song|
+      return song if song.name == name
+    end
+    return nil
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name[0]}
+  end
+
+  def self.parse(filename)
+    # parses filename in .mp3
+    #returns an array with [artist, name]
+    info = []
+    parsed = filename.split(" - ")
+    info << parsed[0]
+    info << parsed[1].chomp(".mp3")
+    info
+  end
+  def self.new_from_filename(filename)
+    #input: " -.mp3 "
+    song = self.new_by_name(self.parse(filename)[1])
+    song.artist_name = self.parse(filename)[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.create_by_name(self.parse(filename)[1])
+    song.artist_name = self.parse(filename)[0]
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+
+
+
+
 
 end
