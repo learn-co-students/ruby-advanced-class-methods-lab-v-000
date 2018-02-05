7d916cded44d65668e1fb8adc9c7bd7f4695dd85
Brent  Ransom <sadumedusa@gmail.com>
2018-02-05 16:46:49 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,54 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(name)
+    arry = name.split(" - ")
+    song = self.new
+    song.name = arry[1].split(".")[0]
+    song.artist_name = arry[0]
+    song
+  end
+
+  def self.create_from_filename(name)
+    song = self.new_from_filename(name)
+    artist_name = self.new_from_filename(name)
+    @@all << song
+    @@all << artist_name
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
 end
