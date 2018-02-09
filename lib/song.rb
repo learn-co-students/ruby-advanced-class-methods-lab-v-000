88d96bb157095483c9ce77793572a5e4cf58b46b
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-02-09 02:16:45 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,12 +10,15 @@ class Song
     self.class.all << self
   end
 
-  def self.create(name)
+  def self.create
     song = self.new
-    song.name = name
     @@all << song
+    song
+  end
 
-
+  def self.new_by_name
+    song = self.new
+    @@all << song
   end
 
 end
