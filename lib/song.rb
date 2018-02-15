ba923f3caa9ad1204d84ea77f533499c0bbd44aa
Aderianna T Williams <aderiannaw@yahoo.com>
2018-02-15 01:10:02 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,27 @@ class Song
   def self.all
     @@all
   end
+  
+  def initialize(name)
+    @create = create
+    @new_by_name = new_by_name
+  end
+  
+  def self.create
+    @@all
+  end
+  
+  def self.new_by_name(name)
+    @@all
+  end
+  
+  def self.find_by_name(name)
+    @@self.detect{|song| song.name == name} 
+  end
 
   def save
     self.class.all << self
   end
+
 
 end
