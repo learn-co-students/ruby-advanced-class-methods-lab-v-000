9a2dfe19e1e079ed304b1682b73ed8443e06aaf4
Veronica Kim <vero.a.kim@gmail.com>
2018-02-18 05:44:40 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,45 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|person| person.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) != nil
+      return self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    @@all.detect{|person| person.name == name}
+  end
+
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+
 end
