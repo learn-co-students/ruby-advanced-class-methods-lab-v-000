b880c75c082beadee25f2eddb997bf12168a7d2c
Veronica Kim <vero.a.kim@gmail.com>
2018-02-18 05:48:33 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -41,9 +41,9 @@ class Song
     end
   end
 
-  def self.find_or_create_by_name(name)
-    @@all.detect{|person| person.name == name}
-  end
+  # def self.find_or_create_by_name(name)
+  #   @@all.detect{|person| person.name == name}
+  # end
 
 
   def self.destroy_all
