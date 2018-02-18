1142cbc552f0cebe902d03eedfef35e387b39882
Jayon Kim <jpkim921@gmail.com>
2018-02-18 20:25:15 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,53 +11,9 @@ class Song
   end
 
   def self.create
-    self.all << Song.new
-    self.all.last
-  end
-
-
-  def self.new_by_name(name)
-      song = self.new
-      song.name = name
-      song
-    end
-
-  def self.create_by_name(name)
-    self.create.name = name
-    self.all.last
-  end
-
-  def self.find_by_name(name)
-    self.all.detect{|title| title.name == name}
-  end
-
-  def self.find_or_create_by_name(name)
-    if self.find_by_name(name) != nil
-      return self.find_by_name(name)
-    else
-      self.create_by_name(name)
-    end
-  end
-
-  def self.alphabetical
-      self.all.sort_by {|instance| instance.name}
-  end
-
-  class Song
-  attr_accessor :name, :artist_name
-  @@all = []
-
-  def self.all
-    @@all
-  end
-
-  def save
-    self.class.all << self
-  end
-
-  def self.create
-    self.all << Song.new
-    self.all.last
+    song = self.new
+    self.all << song
+    song
   end
 
 
@@ -90,16 +46,28 @@ class Song
 
   def self.new_from_filename(filename)
     arr = filename.split(" - ")
+    artist = arr[0]
+    name = arr[1]
+    name = name[0...-4]
+    song = self.new
+    song.name = name
+    # song = self.create_by_name(name)
+    song.artist_name = artist
+    song
+  end
 
-    name = arr[0]
-    arr[1] =
-    artist = arr[1]
-    # artist = arr[1].match(/^([^.]+)/)
+  def self.create_from_filename(filename)
+    arr = filename.split(" - ")
+    artist = arr[0]
+    name = arr[1]
+    name = name[0...-4]
     song = self.create_by_name(name)
     song.artist_name = artist
     song
   end
 
-end
+  def self.destroy_all
+    self.all.clear
+  end
 
 end
