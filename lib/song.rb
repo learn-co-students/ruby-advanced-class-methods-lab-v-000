5ffb367bb945fc1523328cb811276d78334a99f4
Jayon Kim <jpkim921@gmail.com>
2018-02-18 05:14:12 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,37 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    self.all << Song.new
+    self.all.last
+  end
+
+
+  def self.new_by_name(name)
+      song = self.new
+      song.name = name
+      song
+    end
+
+  def self.create_by_name(name)
+    self.create.name = name
+    self.all.last
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|title| title.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) != nil
+      return self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+
+  end
+
 end
