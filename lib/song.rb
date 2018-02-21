5326b4d6d9ea5595e3e92ac278b12c83fdf91b8f
Harley Rayner <harleymteden@gmail.com>
2018-02-21 04:50:09 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,25 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+
+  def self.create_by_name(name)
+    song = Song.create
+    song.name = name
+    song
+  end
+
+  
+
 end
