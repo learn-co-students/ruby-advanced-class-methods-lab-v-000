744eae77e6d86034927961bfd1f5eb478bf483bc
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-28 18:40:06 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,42 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    new_song = self.new
+    @@all << new_song
+    new_song
+  end
+
+  def self.new_by_name(name)
+    
+  end
+
+  def self.create_by_name
+
+  end
+
+  def self.find_by_name
+
+  end
+
+  def self.find_or_create_by_name
+
+  end
+
+  def self.alphabetical
+
+  end
+
+  def self.new_from_filename
+
+  end
+
+  def self.create_from_filename
+
+  end
+
+  def self.destroy_all
+
+  end
+
 end
