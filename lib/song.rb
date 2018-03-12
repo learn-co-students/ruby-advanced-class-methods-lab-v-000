6dd52acb95e34f05694449b3b4dac1e50d76baec
Katie Cassell <kcassell944@gmail.com>
2018-03-12 18:47:57 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -38,13 +38,21 @@ def self.alphabetical
 end
 
 def self.new_from_filename(filename)
-  split_string = filename.split(" - ")  
+  split_string = filename.split(" - ")
+  split_string[1].slice!(".mp3")
   song = self.new
-  name = self.new
-  song.name = name
-  song.artist_name = artist
+  song.name = split_string[1]
+  song.artist_name = split_string[0]
+  song
 end
 
+def self.create_from_filename(filename)
+  @@all << self.new_from_filename(filename)
+end
+
+def self.destroy_all
+  @@all = []
+end
 
   def save
     self.class.all << self
