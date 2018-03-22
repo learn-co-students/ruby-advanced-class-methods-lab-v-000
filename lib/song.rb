5104074e91b23e19a3336b1a575ac409c5cd19e8
Victoria Meng <nights1ststar@protonmail.com>
2018-03-22 23:01:43 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,19 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
 end
+
+song = Song.new_by_name("Blank Space")
+song
+song.name
