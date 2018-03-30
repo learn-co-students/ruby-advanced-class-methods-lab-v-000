8b9e9f5f8b6a0c8745d6b276b32c579c170f6e7a
Karen G <kgraves0701@gmail.com>
2018-03-30 16:21:36 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -12,21 +12,21 @@ class Song
 
   def self.create
     song = self.new
-    @@all << song
+    song.save
     song
   end
 
   def self.new_by_name(name)
     song = self.new
     song.name = name
-    @@all << song
+    song.save
     song
   end
 
   def self.create_by_name(name)
     song = self.new
     song.name = name
-    @@all << song
+    song.save
     song
   end
 
@@ -59,7 +59,7 @@ class Song
 
   def self.create_from_filename(filename)
     song = self.new_from_filename(filename)
-    @@all << song
+    song.save
   end
 
   def self.destroy_all
