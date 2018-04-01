159247630f1e1b292935e4bda179d8c67a59ad53
Lauren Pardee <lap5151@gmail.com>
2018-04-01 14:34:58 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,69 @@
+require'pry'
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
     @@all
+  end
+
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all <<song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.detect {|match| name == match.name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by{|key| key.name}
+  end
+
+  def self.new_from_filename(filename)
+    song = self.new
+    split_filename = filename.split(" - ")
+    song_name = split_filename[1].split(".")
+    song.name = song_name[0]
+    song.artist_name = split_filename[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new
+    split_filename = filename.split(" - ")
+    song_name = split_filename[1].split(".")
+    song.name = song_name[0]
+    song.artist_name = split_filename[0]
+    self.all << song
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
   def save
