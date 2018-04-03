967eb419700db4c9e27e96728289759df4518125
Jonathan Williams <jtw365@live.missouristate.edu>
2018-04-03 21:45:24 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,20 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    name
+    #binding.pry
+  end
+
+
   def self.all
     @@all
   end
