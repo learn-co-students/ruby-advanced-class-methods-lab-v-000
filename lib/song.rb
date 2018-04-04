28013ed8be5f75444a4f255d9812a6326c25edd3
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-04-04 02:00:07 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,12 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  # def initialize(name, artist_name)
+  #   @name = name
+  #   @artist_name = artist_name
+  #   @@all << self
+  # end
+
   def self.all
     @@all
   end
@@ -10,4 +16,52 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    song = @@all.detect {|s| s.name == name}
+    song
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|s| s.name}
+  end
+
+  def self.new_from_filename(filename)
+    fn = filename.split(".mp3").join
+    song = self.new
+
+    song.artist_name = fn.split(" - ")[0]
+    song.name = fn.split(" - ")[1]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).save
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
 end
--- spec/song_class_methods_spec.rb
+++ spec/song_class_methods_spec.rb
@@ -32,7 +32,7 @@ describe "Song Class Methods" do
       song_1 = Song.create_by_name("Blank Space")
       song_2 = Song.create_by_name("Hello")
       song_3 = Song.create_by_name("Hotline Bling")
-
+      
       expect(Song.find_by_name("Hello")).to eq(song_2)
     end
   end
@@ -51,7 +51,7 @@ describe "Song Class Methods" do
 
       expect(song_1).to eq(song_2)
     end
-    
+
     it 'creates a new Song object with the provided title if one doesn\'t already exist' do
       blank_space = Song.find_by_name("Blank Space")
       expect(blank_space).to be(nil)
