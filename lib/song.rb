f01b8d7d1a1deac8d635b4258a137424948a0ad7
Jonathan Williams <jtw365@live.missouristate.edu>
2018-04-04 17:23:50 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,9 +11,55 @@ class Song
   def self.new_by_name(name)
     song = self.new
     song.name = name
-    name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|artist| artist.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.create_by_name(name) || self.find_by_name(name)
+    self.find_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
     #binding.pry
   end
+
+  def self.new_from_filename(filename)
+    song = self.new
+    new = filename.split(/\s-\s+|\.+\m+\p+\d/)
+    song.name = new[1]
+    song.artist_name = new[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new
+    #new = filename.split(/\s-\s/)
+    new = filename.split(/\s-\s+|\.+\m+\p+\d/)
+    song.name = new[1]
+    song.artist_name = new[0]
+    @@all << song
+    song
+    #binding.pry
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+
+
 
 
   def self.all
