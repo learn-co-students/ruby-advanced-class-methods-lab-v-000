94ca3cbabbcbd6a914fa43f08249f43507bc9b6b
ecssiah <ecssiah@gmail.com>
2018-04-14 02:20:49 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -33,8 +33,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    song = self.find_by_name(name)
-    song ? song : self.create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
   def self.alphabetical
