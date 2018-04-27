f807ee46a118d8ad86588a08e5a3995d28ee2c68
Ji Yim <gsooyim@gmail.com>
2018-04-27 20:07:48 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
   end
 
   def save
-    self.class.all << self
+    @@all << self
   end
   
   def self.create
@@ -23,9 +23,8 @@ class Song
   end
   
   def self.create_by_name(name)
-    song = self.new
+    song = self.create
     song.name = name
-    @@all << song
     song
   end
   
