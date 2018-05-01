dc40c37475ce04bc820d2fcf0fa58ecdab8412bb
Megan Collier <meganraec@live.com>
2018-05-01 02:53:22 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -16,32 +16,32 @@ class Song
     self.class.all << self
   end
 
-  def self.new_by_name(name)
+  def self.new_by_name(title)
     song = Song.new
-    song.name = name
+    song.name = title
     song
   end
 
-  def self.create_by_name(name)
-    song = Song.new_by_name(name)
+  def self.create_by_name(title)
+    song = Song.new_by_name(title)
     song.save
     song
   end
 
-  def self.find_by_name(name)
-    self.all.find {|x| x.name == name}
+  def self.find_by_name(title)
+    self.all.find {|x| x.name == title}
   end
 
-  def self.find_or_create_by_name(name)
-    if self.find_by_name(name)
-      self.find_by_name(name)
+  def self.find_or_create_by_name(title)
+    if self.find_by_name(title)
+      self.find_by_name(title)
     else
-      Song.create_by_name(name)
+      Song.create_by_name(title)
     end
   end
 
   def self.alphabetical
-    @@all.sort
+    self.all.sort_by {|title| title }
   end
 
   def self.new_from_filename
