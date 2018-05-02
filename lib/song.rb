193b655824d687b999bf47251b013a90eb825289
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-02 23:15:05 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,47 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    newsong = self.new
+    self.all << newsong
+    newsong
+
+  end
+
+  def self.new_by_name(name)
+    s = self.new
+    s.name = name
+    s
+  end
+
+  def self.create_by_name(name)
+    s = self.new
+    s.name = name
+    self.all << s
+    s
+  end
+
+  def self.find_by_name(name)
+    self.all.detect do |instance|
+      instance.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    is_it_there = self.find_by_name
+      if is_it_there == nil
+        self.create_by_name
+      else
+        is_it_there
+      end
+  end
+
+
+
+  def self.destroy_all
+    @@all.clear
   end
 
 end
