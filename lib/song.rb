a08881fff4adaa1f38f390ba0bb7a1dce7185589
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-20 03:36:33 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,6 @@
+require 'pry'
+# binding.pry
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -11,10 +14,48 @@ class Song
   end
 
   def self.create
-    song = self.new
-    self.new
     new.save
-    song
+    self.all[-1]
+  end
+
+  def scope_title(title)
+    self.name = title
+    save
+  end
+
+  def self.new_by_name(string)
+    new.scope_title(string)
+    song = self.all.pop
+  end
+
+  def self.create_by_name(string)
+    new.scope_title(string)
+    self.all[-1]
+  end
+
+  def title_find(search)
+    self.class.all.find { |title|
+      return title if title.name == search
+    }
+  end
+
+  def self.find_by_name(string)
+    result = new.title_find(string)
+  end
+
+  def self.find_or_create_by_name
+  end
+
+  def self.alphabetical
+  end
+
+  def self.new_from_filename
+  end
+
+  def self.create_from_filename
+  end
+
+  def self.destroy_all
   end
 
 end
