24e97d6880494b26b9c71529304a6041c5e337d3
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-21 20:44:58 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,3 @@
-
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -12,70 +11,56 @@ class Song
   end
 
   def self.create
-    new.save
-    self.all[-1]
+    song = Song.new
+    song.save
+    song
   end
 
-  def scope_title(title, musician = nil)
-    self.name = title
-    self.artist_name = musician
-    save
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
   end
 
-  def self.new_by_name(string)
-    new.scope_title(string)
-    self.all.pop
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
   end
 
-  def self.create_by_name(string)
-    new.scope_title(string)
-    self.all[-1]
+  def self.find_by_name(song_name)
+    self.all.find { |title| title.name == song_name }
   end
 
-  def title_find(search)
-    self.class.all.find { |title|
-      return title if title.name == search
-    }
-  end
-
-  def self.find_by_name(string)
-    new.title_find(string)
-  end
-
-  def self.find_or_create_by_name(string)
-    result = find_by_name(string)
-    if result != nil
-      result
-    else
-      create_by_name(string)
-    end
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
   end
 
   def self.alphabetical
-    self.all.sort_by { |music|
-      music.name
-    }
+    self.all.sort_by { |music| music.name }
   end
 
-  def self.new_from_filename(string)
-    musician = string.match(/.+\b -/).to_s[0...-2]
-    title = string.match(/- .+.mp3/i).to_s[2...-4]
-    new.scope_title(title, musician)
-    self.all.pop
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    musician = parts[0]
+    title = parts[1].gsub(".mp3", "")
+    song = self.new
+    song.artist_name = musician
+    song.name = title
+    song
   end
 
-  def self.create_from_filename(string)
-    musician = string.match(/.+\b -/).to_s[0...-2]
-    title = string.match(/- .+.mp3/i).to_s[2...-4]
-    new.scope_title(title, musician)
-    self.all[-1]
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    musician = parts[0]
+    title = parts[1].gsub(".mp3", "")
+    song = self.create
+    song.artist_name = musician
+    song.name = title
+    song
   end
 
   def self.destroy_all
-    @@all.clear
+    self.all.clear
   end
 end
-
-
-
-# spacing
