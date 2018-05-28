7c484a778ba8e2eb712863d91d86fdca7806268c
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-28 17:30:23 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,14 @@
 class Song
+
   attr_accessor :name, :artist_name
+
   @@all = []
+
+  def initialize
+    @name = name
+    @artist_name = artist_name
+    @@all << self
+  end
 
   def self.all
     @@all
@@ -8,6 +16,55 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) || self.create_by_name(name)
+      return self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by! {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    song = self.new
+    song.name = filename.split(/[^a-zA-Z\s]|\s-\s/)[1]
+    song.artist_name = filename.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    song
+  end
+
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
