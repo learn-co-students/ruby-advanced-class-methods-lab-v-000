db121e06df27bc5e78aee8a85c6230f3bb3defca
Aziz Kosber <a.kosber@gmail.com>
2018-06-06 16:09:50 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,27 @@ class Song
     self.class.all << self
   end
 
+  def self.create #self referes to the class. this is a class method
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.new_by_name(name)
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect do |song|
+      song.name == name
+    end
+  end
 end
