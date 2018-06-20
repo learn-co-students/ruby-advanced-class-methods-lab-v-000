6e2eecceb2352453813ce9b854ba7bf56e35cb77
Karyme Virginia <aveonyxcode@gmail.com>
2018-06-20 16:25:14 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,25 +11,25 @@ class Song
   end
 
   def self.create
-    new_song = self.new
+    new_song = new
     new_song.save
     new_song
   end
 
   def self.new_by_name(name)
-    new_song = self.new
+    new_song = new
     new_song.name = name
     new_song
   end
 
   def self.create_by_name(name)
-    new_song = self.new_by_name(name)
+    new_song = new_by_name(name)
     new_song.save
     new_song
   end
 
   def self.find_by_name(name)
-    self.all.find { |s| s.name == name }
+    all.find { |s| s.name == name }
   end
 
   def self.find_or_create_by_name(name)
@@ -42,23 +42,23 @@ class Song
   end
 
   def self.alphabetical
-    self.all.sort { |a, b| a.name <=> b.name }
+    all.sort { |a, b| a.name <=> b.name }
   end
 
   def self.new_from_filename(filename)
     song = filename.split(/[\.-]/)
-    new_song = self.new
+    new_song = new
     new_song.name = song[1].strip
     new_song.artist_name = song[0].strip
     new_song
   end
 
   def self.create_from_filename(filename)
-    new_song = self.new_from_filename(filename)
+    new_song = new_from_filename(filename)
     new_song.save
   end
 
   def self.destroy_all
-    self.all.clear
+    all.clear
   end
 end
