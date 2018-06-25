68191d41a7cfe51907e310f8a2926077b5d24536
Michael Blandford <mdblandford@gmail.com>
2018-06-25 20:05:05 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,57 +9,57 @@ class Song
   def save
     self.class.all << self
   end
-  
+
   def self.create
-    song = self.new 
-    song.save
-    song 
-  end
-  
-  def self.new_by_name(songName)
     song = self.new
-    song.name = songName
+    song.save
     song
   end
-  
-  def self.create_by_name(songName)
-    song = self.create  
-    song.name = songName
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
     song
   end
-  
-  def self.find_by_name(songName)
-    self.all.detect {|song| song.name == songName}
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
   end
-  
-  def self.find_or_create_by_name(songName)
-    find_by_name(songName) || create_by_name(songName)
+
+  def self.find_by_name(name)
+    self.all.detect {|song| song.name == name}
   end
-  
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create_by_name(name)
+  end
+
   def self.alphabetical
     self.all.sort_by {|song| song.name}
   end
-  
+
   def self.new_from_filename(filename)
     title = filename.split(" - ")
     artist = title[0]
-    songName, extension = title[1].split(".")
-    song = self.new 
+    name, extension = title[1].split(".")
+    song = self.new
     song.artist_name = artist
-    song.name = songName
+    song.name = name
     song
   end
-  
+
   def self.create_from_filename(filename)
     title = filename.split(" - ")
     artist = title[0]
-    songName, extension = title[1].split(".")
-    song = self.create 
+    name, extension = title[1].split(".")
+    song = self.create
     song.artist_name = artist
-    song.name = songName
+    song.name = name
     song
   end
-  
+
   def self.destroy_all
     @@all.clear
   end
