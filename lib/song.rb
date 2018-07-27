f6fb431a671c68bf7bcf5612d7a232c6fdcf0f5a
Dana Palmer <roiai4@hotmail.com>
2018-07-27 08:04:09 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +10,21 @@ class Song
   def save
     self.class.all << self
   end
+  
+  #Class method here.  Instead of initialize, we are adding this to our class
+  #functionality.
+  #To Define this method we enact the following
+  # the local var song is = Song.new, which is creating a new song
+  # the song is then saved by song.save?? and then returns the song
+  def self.create
+    song = Song.new
+    song.save
+    song
+end
 
+def self.new_by_name(name)
+  @name = Song.new 
+  @name.save 
+  @name
+end
 end
