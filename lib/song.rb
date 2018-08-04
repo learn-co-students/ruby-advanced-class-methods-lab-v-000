cf215f36d562a583ed32482e9db810bfb70ad060
Anna Dybas <a.m.dybas@gmail.com>
2018-08-04 20:05:05 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,32 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+  end
+
+  def self.new_by_name
+  end
+
+  def self.create_by_name
+  end
+
+  def self.find_by_name
+  end
+
+  def self.find_or_create_by_name
+  end
+
+  def self.alphabetical
+  end
+
+  def self.new_from_filename
+  end
+
+  def self.create_from_filename
+  end
+
+  def self.destroy_all
+    self.all.clear #why not self.class.clear?
+  end
+
 end
