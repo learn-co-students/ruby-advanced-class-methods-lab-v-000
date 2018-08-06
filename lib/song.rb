a2d987111add0388075f069f3c6e82c712f9386a
Anna Dybas <a.m.dybas@gmail.com>
2018-08-06 18:45:44 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,12 +11,21 @@ class Song
   end
 
   def self.create
+    song = self.new
+    song.save
+    song
   end
 
-  def self.new_by_name
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
   end
 
-  def self.create_by_name
+  def self.create_by_name(song_name)
+    song = self.new
+    self.create
+    song
   end
 
   def self.find_by_name
