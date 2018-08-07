c207d5995fdbe7cb7b3a8e7aba13ba4295f137e7
Sharmaine Requerme <srqrme@gmail.com>
2018-08-07 14:44:56 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -38,4 +38,32 @@ class Song
   end
 
   def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    file = filename.split(" - ")
+    artist_name = file[0]
+    song_name = file[1].gsub(".mp3", "")
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    file = filename.split(" - ")
+    artist_name = file[0]
+    song_name = file[1].gsub(".mp3", "")
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
