241e154081bc32c6951779df8de97ccfa0017f10
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-08 03:12:17 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -32,14 +32,14 @@ class Song
 
 
   def self.find_by_name(name)
-    @@all.find{|person| person.name == name}
+    @@all.find{|song| song.name == name}
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) != false
-      return self.find_by_name(name)
+    if self.find_by_name(name) != nil
+      self.find_by_name(name)
     else
-      return self.create_by_name(name)
+      self.create_by_name(name)
     end
   end
 
