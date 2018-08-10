94defae565d957dfad93a2fe87db05ae2ef267f6
Jaime Johnsen <drivetraindiva@gmail.com>
2018-08-10 01:50:02 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -22,7 +22,7 @@ class Song
   def self.new_by_name(name)
       #make a new instance
       song = self.new       #new instance
-      song.name= name          # call a method on it that will allow us to name it as the song_name
+      song.name= name          # giving the song instance a name attribute. call a method on it that will allow us to name it as the song_name
       song                            #access to an instance of the Song class
     end
 
@@ -31,14 +31,14 @@ class Song
 #returns a song instance with that name set as its name property
   def self.create_by_name(name) #takes in a string
   #make a new instance
-  song = self.create
-  song.name = name #what is line 35 doing exactly?
+  song = self.create # returns an object that stored in the class variable  or the array
+  song.name = name #setting the saved object name attribute
   song #return new song
   end
 #this is a class finder
   def self.find_by_name(name) #accept a string
     #I need to iterate over @@all to check for matching song names
-  self.all.find { |song| song.name == name }
+  self.all.find { |song| song.name == name }# checking each song objects name attribute in the array
   end
 
   def self.find_or_create_by_name(name)
@@ -52,17 +52,18 @@ class Song
   def self.new_from_filename(filename)  #this ia a class constructor. A constructor 1.Allocates space for the object
     song = self.new #new instance       #2.assigns instance variables their initial value #3. Returns the instance of that class
     split_filename = filename.chomp(".mp3").split(" - ") #need to split filename up and remove the end of string
+binding.pry
     song.name = split_filename[1]
     song.artist_name = split_filename[0]
     song
   end
 
   def self.create_from_filename(filename) #this is a class constructor
-    song = self.create
+    song = self.create #returns the instance of Song which has an object ID
     split_filename = filename.chomp('.mp3').split(" - ") #need to split filename up and remove the end of string
-    song.name = split_filename[1]
+    song.name = split_filename[1] # setting the name attribute equal to the element at the index in split_filename
     song.artist_name = split_filename[0]
-    song  # how are lines 63 and 64 returned by calling song on this line??
+    song  # returns object Id wich now has the attributes that we set in line 64 and 65
   end
 
   def self.destroy_all
