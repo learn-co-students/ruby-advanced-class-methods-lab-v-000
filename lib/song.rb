4328d4fb2d057e473c9358899f859ce10d5e8b4d
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-08-10 02:37:24 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
   def self.all
     @@all
   end
@@ -9,5 +9,12 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    @self = self.new
+    @self.save
+    @self
+  end
+
 
 end
