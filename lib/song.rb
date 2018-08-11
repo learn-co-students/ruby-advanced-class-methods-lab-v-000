c28d336713abe0bc818ffaa8556648f6fdb569de
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-08-11 20:58:19 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
     @self
   end
   
-   def self.new_by_name(name)
+  def self.new_by_name(name)
     @self.name = name
     @self
   end
@@ -28,18 +28,12 @@ class Song
     @self
   end
   
-  def self.find_by_name(name)
-    self.all.find { |song_name| song_name.name == name }
+  def self.find_by_name(song)
+    binding.pry
+    self.all.find{|title| title.name == song}
   end
+  
 
-  def self.find_or_create_by_name(name)
-    self.all.each do |x|
-      if x.name == name
-        self.find_by_name
-      else 
-        self.create_by_name
-      end
-    end
-   end     
+     
         
 end
