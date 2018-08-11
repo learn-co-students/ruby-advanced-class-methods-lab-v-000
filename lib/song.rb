8da482ad6491b3ba5ec7172fc809bf56aaa4c8f7
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-08-11 04:45:20 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -15,6 +15,21 @@ class Song
     @self.save
     @self
   end
+  
+   def self.new_by_name(name)
+    @self.name = name
+    @self
+  end
+  
+  def self.create_by_name(name)
+    @self.name = name
+    @self.save
+    @self
+  end
+  
+  def self.find_by_name(name)
+    @self.all.find do { |song_name| song_name.name == name }
 
-
+  end
+end
 end
