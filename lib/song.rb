10d8b2447e91d902d3f375fa0eccc12a0be6829f
Scott Hall <scotthall127@gmail.com>
2018-08-12 19:56:28 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,17 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+  end
+
+
 end
