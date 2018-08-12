87c8fd84b83b7710ab111d70b9882c42f043f097
Scott Hall <scotthall127@gmail.com>
2018-08-12 20:11:17 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -22,5 +22,26 @@ class Song
     @@all << song
   end
 
+  def self.create_by_name
+  end
+
+  def self.find_by_name
+  end
+
+  def find_or_create_by_name
+  end
+
+  def self.alphabetical
+  end
+
+  def self.new_from_filename
+  end
+
+  def self.create_from_filename
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 
 end
