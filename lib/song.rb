1df5280f133cb0509b9ebbdff67c8c216a19ae13
Rafael Lopez <lpz.raf@gmail.com>
2018-08-19 02:06:37 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -34,16 +35,23 @@ class Song
     end
   end
     
-  # def self.find_or_create_by_name(song_name)
-  #   if find_by_name == find_by_name
-  #   find_by_name
-  #   elsif
-  #   create_by_name
-  #   end
-  # end
+  def self.find_or_create_by_name(song_name)
+   self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
   
   def self.alphabetical
     @@all.sort_by { |change| change.name }
+  end
+  
+  def self.new_from_filename(mp3_song)
+  binding.pry
+  artist = song.artist_name
+  song = song.name
+  song
+  end
+  
+  def self.create_from_filename(mp3_song)
+  
   end
   
   def self.destroy_all
