69c52360f277e30ee313667c0b5d2d994ec38ab5
Ashley Wong <ashwong04@gmail.com>
2018-09-12 01:13:21 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,22 @@ class Song
     self.class.all << self
   end
 
+  def create(song)
+    def initialize
+      @@all << self.class.new
+    end
+  end
+
+
+
+
+
+  def new_byName(name)
+    if @@all.include?(name)
+      return name
+    end
+  end
+
+
+
 end
