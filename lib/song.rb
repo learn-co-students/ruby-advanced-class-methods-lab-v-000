7c4f72f8410a72bc7d814ac58d7b1dae7d3dc581
Ashley Wong <ashwong04@gmail.com>
2018-09-15 02:39:52 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,8 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
-  def initialize
-    
-  end
 
   def self.all
     @@all
@@ -45,17 +43,22 @@ class Song
   end
 
   def self.new_from_filename(fileName)
-    split = fileName.split("-")
-    song = self.new
-    song.artist = split[0]
-    name = fileName.split(".")
+    #binding.pry
+    split_file = fileName.split("-")
+    s_name = split_file[1].lstrip.gsub(".mp3", "")
+    song = Song.new
+    song.artist_name = split_file[0].strip
+    song.name = s_name
+    return song
   end
 
+  def self.create_from_filename(fileName)
+    @@all << self.new_from_filename(fileName)
+    @@all.last
+  end
 
-
-
-
-
-
+  def self.destroy_all
+    @@all.clear
+  end
 
 end
