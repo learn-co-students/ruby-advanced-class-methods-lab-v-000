e1478c38544b5b39e22599df22acf905b070ee4e
J Fern <jfern20@gmail.com>
2018-09-25 04:34:38 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -54,7 +54,14 @@ class Song
     song = Song.new
     song.name = split_filename[1]
     song.artist_name = split_filename[0]
+    song.save
     song 
     
+    end 
+    
+    def self.destroy_all
+      self.all.clear
+      # @@all = []
+      
     end 
 end
