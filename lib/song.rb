fd1779ed6301cccff96aecb79b87224252d792e7
Cheryl K <cherylkelson@icloud.com>
2018-09-26 22:19:40 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -34,46 +34,50 @@ class Song
   end
   
   
-    @song = self.create
-    @song.name = name
-    @song
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+    @all
   end
-   
+  
+    
+  
+  def self.find_or_create_by_name(name) 
+      find = self.find_by_name(name)
+        if want 
+          want 
+        else 
+          self.create_by_name(name) 
+            find_by_name 
+            create_by_name
+        song
+         
+        end
   end
  
- 
-  # def self.alphabetical 
-  #   sorted = self.all.sort_by{|song| song.ame}  
-  #   sorted
-  # end 
+  def self.alphabetical 
+    sorted = self.all.sort_by{|song| song.name}  
+    sorted
+  end 
   
   
   # def self.new_from_filename(filename)
   #   song = self.new
   #   song_array = filename.split("","-",".  ") 
-    def Song.find_or_create_by_name(song)
-    want = self.find_by_name(title)
-    if want 
-        want
-    else 
-      self.create_by_name(title)
-    en
   #   song_artist_name = song_array[0]
   #   song_name = song_array [1] 
   # end
   
   
-  # def self.create_from_filename(filename)
-  #   song = self.create
-  #   song_artist_name
-  #   song_name 
+  def self.create_from_filename(filename)
+    song = self.create
+    song_artist_name
+    song_name 
     
-  #   song
-  # end
+    song
+  end
   
   
-  # def self.destroy_all 
-  #   self.all.clear 
-  # end
-  
-
+  def self.destroy_all 
+    self.all.clear 
+  end
+end 
