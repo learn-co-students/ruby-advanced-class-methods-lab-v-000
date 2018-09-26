46c8a6be14a470dc7c71a4c0d34e6b65ce557d04
Cheryl K <cherylkelson@icloud.com>
2018-09-26 21:34:39 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,73 +2,78 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
   
+  
   def self.all
     @@all
   end
  
+  
   def save
     self.class.all << self
   end
  
 
   def self.create 
-    song = self.new 
-    song.all << song
-  end
- 
-  def self.new_by_name(title) 
-    song = self.new 
-    song.name = title 
-    song 
-  end
- 
-  def self.create_by_name(title)
-    song = self.create
-    song.name = title
-    song
-  end
-  
-  def self.find_by_name(title)
-    song = self.create
-    song.name = title
-    song
-  end
-  
-  # def self.find_or_create_by_name(song)
-  #   song = self.find
-  #   if song 
-  #     song 
-  #   else 
-  #   @@song = self.new
-  #   end
- 
-  def self.alphabetical 
-    sorted = self.all.sort_by{|song| song.ame}  
-    sorted
-  end 
-  
-  
-  def self.new_from_filename(filename)
     song = self.new
-    song_array = filename.split("","-",".  ") 
-    
-    song_artist_name = song_array[0]
-    song_name = song_array [1] 
-  end
-  
-  
-  
-  def self.create_from_filename(filename)
-    song = self.create
-    song_artist_name
-    song_name 
-    
+    self.all << song
     song
   end
-  
-  
-  def self.destroy_all 
-    self.all.clear 
+ 
+ 
+  def self.new_by_name(name) 
+    @song = self.new 
+    @song.name = name
+    @song
+  end
+ 
+ 
+  def self.create_by_name(name)
+    @song = self.create  
+    @song.name = name
+    @song
   end
   
-end
+  
+    @song = self.create
+    @song.name = name
+    @song
+  end
+   
+  end
+ 
+ 
+  # def self.alphabetical 
+  #   sorted = self.all.sort_by{|song| song.ame}  
+  #   sorted
+  # end 
+  
+  
+  # def self.new_from_filename(filename)
+  #   song = self.new
+  #   song_array = filename.split("","-",".  ") 
+    def Song.find_or_create_by_name(song)
+    want = self.find_by_name(title)
+    if want 
+        want
+    else 
+      self.create_by_name(title)
+    en
+  #   song_artist_name = song_array[0]
+  #   song_name = song_array [1] 
+  # end
+  
+  
+  # def self.create_from_filename(filename)
+  #   song = self.create
+  #   song_artist_name
+  #   song_name 
+    
+  #   song
+  # end
+  
+  
+  # def self.destroy_all 
+  #   self.all.clear 
+  # end
+  
+
