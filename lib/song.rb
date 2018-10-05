57377343f0897dcdd4c372c70fae62505067c220
Niusha H <niusha.hadziomerovic@gmail.com>
2018-10-05 17:54:21 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,10 @@ class Song
     song
   end
 
-  def self.new_by_name
-    @name
+  def self.new_by_name(song_string)
+    song = self.new
+    song.name = song_string
+    song
   end
 
   def self.all
@@ -23,7 +25,9 @@ class Song
 end
 
 
-  # def self.find_by_name(name)
+  # def self.find_or_create_by_name("name")
   #   self.all.find{|person| person.name == name}
   # end
+  
+  # Build a class constructor Song.new_by_name that takes in the string name of a song and returns a song instance with that name set as its name property. Song.new_by_name should return an instance of Song and not a simple string or anything else. 
   
