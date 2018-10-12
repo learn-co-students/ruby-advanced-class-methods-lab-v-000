c52fd60d1ac63e1fa54331a0508462714ac71381
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-12 20:55:30 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,38 +2,47 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
   
-  def self.create(name)
-    @name = name
-    save
-    name
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+  
+  def self.create
+    song = self.new
+    song.save
+    song
   end
   
   def self.new_by_name(name)
+    song.create.name = name
   end
   
   def self.create_by_name(name)
+    self.create 
+    save
   end
   
   def self.find_by_name(name)
+    @@all.find{|name| name.name == name}
   end
   
   def self.find_or_create_by_name(name)
-    @@all.name.each do |existing|
-      if name == existing
-        "#{existing}"
-      else 
-        @@all << name
-        
-      end
+    if find_by_name(name) == true 
+      self
+    else 
+      create_by_name(name)
     end
   end
   
   def self.alphabetical
-    @@all.name.sort
+    all.name.sort
   end
   
   def self.new_from_filename
-    #(\[".mp3"\b])
+    #(/[".mp3"\b/])
   end
   
   def self.create_from_filename
@@ -43,13 +52,4 @@ class Song
     @@all.clear
     @@all
   end
-
-  def self.all
-    @@all
-  end
-
-  def save
-    self.class.all << self
-  end
-
 end
