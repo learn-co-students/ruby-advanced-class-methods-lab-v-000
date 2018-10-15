2a1d947b1db3171a9419c11dcd0ce8d5acd81296
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-10-15 16:59:07 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :name, :artist_name, :song
   @@all = []
 
   def self.all
@@ -8,6 +8,11 @@ class Song
 
   def save
     self.class.all << self
+  end
+  
+  def self.create(song)
+    save(song)
+    @all
   end
 
 end
