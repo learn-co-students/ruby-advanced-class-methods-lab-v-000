366ba0d8b71b2d532a0eaca8bcf26771a2572003
Paul Fell-Cinoman <swellpf@gmail.com>
2018-10-26 20:04:14 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -18,14 +18,39 @@ class Song
     song = Song.new
     song.save
     song
+ #   binding.pry
   end
   
   def self.new_by_name(name)
-    name=Song.new
-    self.name = name
-    self.name
+    song=Song.new
+    song.name = name
+    song
   end
  
+  def self.create_by_name(name)
+    song=Song.new
+    song.name = name
+    song.save
+    song
+  end
+  # binding.pry
+  
+  def self.find_by_name(name)
+    @@all.detect { |x| x.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+     self.find_by_name(name) || self.create_by_name(name)
+  end
+  
+  def self.alphabetical
+    @@all.sort_by {|x| x.name}
+  end
+  
+  def self.new_from_filename(filename)
+    song = Song.new
+    song.name=filename
+  end
   
   
   
