cd04d8b9e2eacc032a774e21b550445c10128ccf
Yolanda Fay <yolandafay@gmail.com>
2018-11-03 05:15:45 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,64 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new 
+    song.save
+    song 
+  end
+  
+  def self.new_by_name(song_name)
+     song = self.new 
+     song.name = song_name
+     song 
+  end
+  
+  def self.create_by_name(song_name)
+    song = self.new 
+    song.name = song_name
+    song.save 
+    song
+  end 
+  
+  def self.find_by_name(song_name)
+    self.all.find {|song| song.name == song_name}
+  end
+  
+  def self.find_or_create_by_name(song_name)
+    
+    if find_by_name(song_name) != nil 
+      find_by_name(song_name) 
+    else
+      create_by_name(song_name) 
+    end
+  end
+  
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+  
+  def self.new_from_filename(filename_string)
+    #filename_array = filename_string.split(/[.-]/) - need to be able to remove whitespace
+    song = self.new 
+    song.artist_name = filename_string.split(" - ")[0]
+    song.name = filename_string.split(" - ")[1].split(".")[0]
+    song 
+  end
+  
+  def self.create_from_filename(filename_string)
+    new_from_filename(filename_string).save 
+  end
+  
+  def self.destroy_all 
+    self.all.clear 
+  end
+
+  
 end
+
+
+
+
+
+
+
