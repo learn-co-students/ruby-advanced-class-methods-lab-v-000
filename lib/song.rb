e549fcd8d1b25c4e4eb5d00f971543aa6a59677b
Ryan Brady <ryanbrady369@gmail.com>
2018-11-04 21:38:53 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -26,34 +26,50 @@ class Song
     new_song
   end
 
-  def self.create_by_name(name)  #getting undefined method 'name' when test tries to compare with .name which means reader/accessor not working???
+  def self.create_by_name(name)
     new_song = Song.new
-    @@all << new_song
     new_song.name = name
-  end                            #this method also seems to be working with find_by_name, as changing it makes find fail
+    @@all << new_song
+    new_song
+  end
 
   def self.find_by_name(name)
-    @@all.find do |song|
+    @@all.each do |song|
       if song.name == name
-        return song.name
+        return song
       end
     end
+    false
   end
 
-  def self.new_from_filename(name)
-
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
   end
 
-  def self.create_from_filename(name)
+  def self.new_from_filename(file)
+    file = file.split(" - ")
+    file[1] = file[1].slice(0..-5)
+    new_song = Song.new
+    new_song.name = file[1]
+    new_song.artist_name = file[0]
+    new_song
+  end
 
+  def self.create_from_filename(file)
+    file = file.split(" - ")
+    file[1] = file[1].slice(0..-5)
+    new_song = Song.new
+    new_song.name = file[1]
+    new_song.artist_name = file[0]
+    @@all << new_song
   end
 
   def self.destroy_all
-
+    @@all = []
   end
 
   def self.alphabetical
-
+    @@all.sort_by {|curr| curr.name}
   end
 
 end
