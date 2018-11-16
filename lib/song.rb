ad8fa0b5f7370df6a61602bd15d428febf198163
Paula McKinley <pmknyc@optimum.net>
2018-11-16 00:11:53 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,11 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(name)
+      @name = name
+      self.class.all << self
+    end
+
   def self.all
     @@all
   end
@@ -36,9 +41,46 @@ end
 # Class finder
 # accepts string name of song
 # returns matching instance of song with that name
-def self.find_by_name
+  def self.find_by_name(name)
+      self.all.find{|song| song.name == name}
+  end
 
-end
+# prevent creation of duplicate songs
+# accept string name for song
+# return either matching song instance with that name
+# or create new song with the name and return song instance.
+  def self.find_or_create_by_name
+
+  end
+
+# class method returns all songs
+# in ascending (a-z) alphabetical order.
+# Use Array#sort_by.
+  def self.alphabetical
+
+  end
+# return a new Song instance with song name & artist_name set e.g.:
+# Song.new_from_filename("Taylor Swift - Blank Space.mp3")
+# song.name #=> "Blank Space"
+# song.artist_name #=> "Taylor Swift"
+    def self.new_from_filename
+
+    end
+
+# accepts filename in format
+# "Taylor Swift - Blank Space.mp3"
+# parse filename correctly & save Song instance
+    def self.create_from_filename
+
+    end
+
+# class method reset state of @@all
+# to empty array, deleting all previous song instances.
+    def self.destroy_all
 
 # class end below
 end
+Song.create_from_filename("Thundercat - For Love I Come.mp3")
+Song.create_by_name("Blank Space")
+Song.create_by_name("Hello")
+Song.create_by_name("Hotline Bling")
