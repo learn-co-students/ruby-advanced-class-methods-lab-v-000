f7d83bb4986f37eaa1979d8ad9ec08e9c79a15f2
Lois Lee <lois.lee@me.com>
2018-12-28 19:45:05 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,15 @@
-require 'pry'
-
 class Song
 
   attr_accessor :name, :artist_name
-
   @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
 
   def self.create
     song = self.new
@@ -12,91 +17,55 @@ class Song
     song
   end
 
-  def save
-    self.class.all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.new_by_name(song_name)
+  def self.new_by_name(name)
     song = self.new
-    song.name = song_name
+    song.name = name
     song
   end
 
-  def self.create_by_name(song_name)
+  def self.create_by_name(name)
     song = self.new
-    song.name = song_name
+    song.name = name
     song.save
     song
   end
 
-  def self.find_by_name(song_name)
-    self.all.find{|person| person.name == song_name}
+  def self.find_by_name(name)
+    @@all.find {|s| s.name == name}
   end
 
-  def self.find_or_create_by_name(song_name)
-    self.find_by_name(song_name) || self.create_by_name(song_name)
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
   def self.alphabetical
-    self.all.sort_by do |song|
-      song.name
-    end
+    self.all.sort_by {|s| s.name}
   end
-
-  def self.new_from_filename(filename)
-    data = filename.split(" - ")
-      song = self.new
-      song.artist_name = data[0]
-      song.name = data[1].gsub(".mp3", "")
-      song
-  end
-
-=begin
 
   def self.new_from_filename(filename)
     parts = filename.split(" - ")
     artist_name = parts[0]
     song_name = parts[1].gsub(".mp3", "")
-    
+
     song = self.new
     song.name = song_name
     song.artist_name = artist_name
     song
-end
-
-=end
-
-  def self.create_from_filename(filename)
-
-    data = filename.split(" - ")
-    song = self.new
-    song.artist_name = data[0]
-    song.name = data[1].gsub(".mp3", "")
-    song.save
-    song
   end
 
-=begin #there's a difference between mine and the below ... mine passes the test but
-is it sufficient??
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
 
-def self.create_from_filename(filename)
-  parts = filename.split(" - ")
-  artist_name = parts[0]
-  song_name = parts[1].gsub(".mp3", "")
-
-  song = self.create
-  song.name = song_name
-  song.artist_name = artist_name
-  song
-end
-=end
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
 
   def self.destroy_all
     @@all.clear
   end
-
+  
 end
