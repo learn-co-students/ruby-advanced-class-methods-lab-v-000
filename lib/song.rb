2707b107ca1a390975c77b80edd0be486074fce3
David Anderson <matandersonlv@hotmail.com>
2019-01-16 22:19:40 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   end
 
   def self.find_by_name(name)
-    @@all.detect{|song|song.name == name}
+    @@all.detect{ |song| song.name == name }
   end
 
   def self.find_or_create_by_name(name)
@@ -38,7 +38,7 @@ class Song
   end
 
   def self.alphabetical
-    @@all.sort_by{|song|song.name}
+    @@all.sort_by{ |song| song.name }
   end
 
   def self.new_from_filename(full_name)
