3ad1003223550fe6031c0dd117c77dccabd2d9fe
David Anderson <matandersonlv@hotmail.com>
2019-01-16 22:52:49 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -51,7 +51,7 @@ class Song
 
   def self.create_from_filename(filename)
     split_filename = filename.chomp(".mp3").split(" - ")
-    song = Song.new
+    song = self.new
     song.name = split_filename[1]
     song.artist_name = split_filename[0]
     song.save
