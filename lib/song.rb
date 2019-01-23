1493f4b516abcd70ef46e821b2bf94db2ab314ea
Kimberly Aviles <01010110.v@gmail.com>
2019-01-23 06:05:06 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,30 @@ class Song
     self.class.all << self
   end
 
-end
+  def self.create
+    song = self.new #puts a new object into a variable for safekeeping
+    song.save #uses the SAVE method to shovel the saved object
+    song #implicit return of the object itself
+  end
+
+  def self.new_by_name(name)
+    @name = name #assigns the passed in name to the NAME instance variable
+    song = self.new
+    song.name=(name) #makes the object's instance variable NAME accessible
+    #song.save <-----lab didn't ask to save it- oops
+    song
+  end
+
+  def self.create_by_name(name)
+    @name = name #assigns the passed in name to the NAME instance variable
+    song = self.new
+    song.name=(name) #writer to the NAME instance variable
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+
+  end
+
+end #<-----Song class end
