5e5071e1806bb570748ac482268aea73c1563bc2
Drake Rentrop <drakerentrop@gmail.com>
2019-02-26 19:13:57 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,28 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  def self.create
+    s = self.new
+    s.save
+    s
+  end
+  def self.create_by_name(string_name)
+    s = self.new
+    s.name = string_name
+    s.save
+    s
+  end
+  def self.new_by_name(string_name)
+    s = self.new
+    s.name = string_name
+    s
+  end
+  def self.find_by_name(string_name)
+    self.all.detect {|i| i.name == string_name}
+  end
+  def self.find_or_create_by_name(string_name)
+   self.find_by_name(name) || self.create_by_name(name)
+  end
 
   def self.all
     @@all
