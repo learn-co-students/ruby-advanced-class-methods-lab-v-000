83bab4591b306389fc0573e777f6a1aee2c77ae5
Dan Suarez <suarez.daniel101@gmail.com>
2019-03-27 22:12:30 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,22 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    #self.all << name
+  end
+
+  # def self.create(name)
+  #   person = self.new
+  #   person.name = name
+  #   @@all << person
+  # end
 end
