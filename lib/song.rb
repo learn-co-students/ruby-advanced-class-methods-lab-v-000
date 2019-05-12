99e9bbfe16404d08d0f4d15249c7dc8c36d7c372
BillyPittman1993 <billytrading93@gmail.com>
2019-05-12 00:06:30 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,30 +1,50 @@
-class Song
+class Song 
   attr_accessor :name, :artist_name
+  
   @@all = []
-
+  
   def self.all
-    @@all
-  end
-
+    @@all 
+  end 
+  
   def self.create
-    song =self.new
-    song.save
-    song
+    song = self.new 
+    song.save 
+    song 
   end
-
+  
   def self.new_by_name(name)
-    song=self.new
-    song.name = name
+    song = self.new 
+    song.name = name 
     song
   end
-
+  
   def self.create_by_name(name)
-    song=self.create
-    song.name = name
-    song
+    song = self.new 
+    song.name = name 
+    song.save
+    song 
   end
-
-  def self.new_from_filename(filename)
+  
+  def save 
+    self.class.all << self 
+  end 
+  
+  def self.find_by_name(name)
+     @@all.detect{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    output = self.find_by_name(name)
+    return output if output
+    return self.create_by_name(name) 
+  end 
+  
+  def self.alphabetical 
+    @@all.sort{ |alpha, beta| alpha.name <=> beta.name }
+  end
+  
+   def self.new_from_filename(filename)
     result_array = filename.scan(/(.*) - (.*).mp3/)
     if result_array
       song = self.new
@@ -35,32 +55,15 @@ class Song
       return nil
     end
   end
-
-  def self.create_from_filename(filename)
-    song = self.new_from_filename(filename)
-    song.save if song
-    song
+    
+    def self.create_from_filename(filename)
+      song = self.new_from_filename(filename)
+      song.save if song
+      song
+    end
+    
+    def self.destroy_all 
+      @@all.clear
+    end
+    
   end
-
-  def save
-    self.class.all << self
-  end
-
-  def self.find_by_name(name)
-    return @@all.detect {|song| song.name==name }
-  end
-
-  def self.find_or_create_by_name(name)
-    result = self.find_by_name(name)
-    return result if result
-    return self.create_by_name(name)
-  end
-
-  def self.alphabetical
-    @@all.sort {|i,j| i.name <=> j.name}
-  end
-
-  def self.destroy_all
-    @@all.clear
-  end
-end
