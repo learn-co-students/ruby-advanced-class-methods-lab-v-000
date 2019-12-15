43108ec63af1c28e6ae7fd6858a972c33c491ceb
Ian Gutwinski <iangutwinski@gmail.com>
2019-12-15 18:18:39 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
   end
 
   def save
-    self.class.all << self
+    self.class.all << self #only way a song will be added is through this method
   end
 
   def self.create
@@ -16,50 +16,53 @@ class Song
     song
   end
 
-  def self.new_by_name(song_name)
+  def self.new_by_name(name)
     song = self.new
-    song.name = song_name
+    song.name = name
     song
   end
-  def self.create_by_name(song_name)
-    song = self.create
-    song.name = song_name
-    song
-  end
-  def self.find_by_name(song_name)
-    self.all.find do |song|
-      song.name == song_name
-    end
-    end
 
-  def self.find_or_create_by_name(song_name)
-    if self.find_by_name(song_name)
-      self.find_by_name(song_name)
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    song = @@all.detect {|i| i.name == name }
+    song
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
     else
-       self.create_by_name(song_name)
-     end
+      self.find_by_name(name)
+    end
   end
+
   def self.alphabetical
-    self.all.sort_by {|song| song.name}
+    @@all.sort_by {|a| a.name }
   end
 
-def self.new_from_filename(filename)
-  song = self.new
-  song_info = filename.split(".")[0]
-  song.name = song_info.split(" - ")[1]
-  song.artist_name = song_info.split(" - ")[0]
-  song
-end
+  def self.new_from_filename(filename)
+    name = filename.split(/[\-,.]/)[1].strip
+    artist_name = filename.split(/[\-,.]/)[0].strip
+    song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    song
+  end
 
-def self.create_from_filename(filename)
-  song = self.create
-  song_info = filename.split(".")[0]
-  song.name = song_info.split(" - ")[1]
-  song.artist_name = song_info.split(" - ")[0]
-  song
-end
-def self.destroy_all
-  self.all.clear
-end
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
 
 end
