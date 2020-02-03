e3b91310a339b6e355c66e0ac53e5482e2c89c4f
Julie  McManus <julieannesays@gmail.com>
2020-02-03 20:49:48 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,8 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
-  def self.all
-    @@all
-  end
-
-  def save
-    self.class.all << self
-  end
-
-  def self.create
-    song = Song.new
-    song.save
-    song
-  end
 
   def self.new_by_name(song_name)
     song = self.new
@@ -22,48 +10,62 @@ class Song
     song
   end
 
+  def self.create
+    c = self.new
+    c.save
+    c
+  end
+
   def self.create_by_name(song_name)
-    song = self.create
+    song = self.new
     song.name = song_name
+    song.save
     song
   end
 
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
   def self.find_by_name(song_name)
-    self.all.detect{|s| s.name == song_name}
+    all.detect{|song| song.name == song_name}
   end
 
   def self.find_or_create_by_name(song_name)
-    self.find_by_name(song_name) || self.create_by_name(song_name)
+    if find_by_name(song_name) == nil
+      create_by_name(song_name)
+    else find_by_name(song_name)
+    end
   end
 
   def self.alphabetical
-    self.all.sort_by{|s| s.name}
+    all.sort_by {|obj| obj.name}
   end
 
   def self.new_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].gsub(".mp3", "")
-
+    song_name = filename.split(" - ")[1].chomp(".mp3")
+    artist = filename.split(" - ")[0]
     song = self.new
     song.name = song_name
-    song.artist_name = artist_name
+    song.artist_name = artist
     song
   end
 
   def self.create_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].gsub(".mp3", "")
-
-    song = self.create
+    song_name = filename.split(" - ")[1].chomp(".mp3")
+    artist = filename.split(" - ")[0]
+    song = self.new
     song.name = song_name
-    song.artist_name = artist_name
+    song.artist_name = artist
+    song.save
     song
   end
 
   def self.destroy_all
-    self.all.clear
+    all.clear 
   end
-
 end
