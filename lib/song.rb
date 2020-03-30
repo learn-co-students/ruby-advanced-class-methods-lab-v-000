0c75d4a82a75b301f81b98c44c6a49b2e19aa924
Lamia Haider <lamia.haider@gmail.com>
2020-03-30 06:55:44 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
@@ -10,10 +11,11 @@ class Song
     self.class.all << self
   end
 
+
   def self.create
-     song = self.new
-     song.save
-     song
+    song = self.new #new object
+    song.save
+    song
   end
 
   def self.new_by_name(name)
@@ -21,43 +23,44 @@ class Song
     song.name = name
     song
   end
-
   def self.create_by_name(name)
-    song = self.create
+    song = self.new
     song.name = name
+    song.save
     song
   end
 
   def self.find_by_name(name)
-    self.all.detect do |song|
-      song.name == name
+    self.all.find do |song| song.name == name
     end
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name)
-      return self.find_by_name(name)
-    else self.create_by_name(name)
-    end
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
   def self.alphabetical
-    self.all.sort_by{|song| song.name}
+    self.all.sort_by do |song| song.name
+    end
   end
 
   def self.new_from_filename(filename)
     song = self.new
-    details = filename.chomp(".mp3").split(" - ")
-    song.name = details[1]
-    song.artist_name = details[0]
+    info =  filename.chomp(".mp3").split(" - ")
+    song.name = info[1]
+    song.artist_name = info[0]
     song
   end
 
   def self.create_from_filename(filename)
     self.new_from_filename(filename).save
+
   end
 
   def self.destroy_all
-    @@all = []
+    self.all.clear
   end
+
+
+
 end
