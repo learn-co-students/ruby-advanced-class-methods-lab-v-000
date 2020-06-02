0e19ead4700e177ba600f139ab3a90d903e2cd7d
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-06-02 14:15:11 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -14,58 +14,56 @@ class Song
 
   def self.create
     song = self.new
-    self.all << song
+    song.save
     song
   end
 
-  def self.new_by_name(song_name)
+  def self.new_by_name(name)
     song = self.new
-    song.name = song_name
+    song.name = name
     song
   end
 
-  def self.create_by_name(song_name)
-    song = self.new
-    song.name = song_name
-    self.all << song
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
     song
   end
 
-  def self.find_by_name(song_name)
-    self.all.detect {|song| song.name == song_name}
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
   end
 
-  def self.find_or_create_by_name(song_name)
-    if self.find_by_name(song_name)
-      self.find_by_name(song_name)
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
     else
-      self.create_by_name(song_name)
+      self.create_by_name(name)
     end
   end
 
   def self.alphabetical
-    self.all.sort_by { |song| song.name}
+    self.all.sort_by {|song| song.name}
   end
 
   def self.new_from_filename(filename)
-    song = self.new
     filename_array = filename.chomp(".mp3").split(" - ")
+    song = self.new
     song.artist_name = filename_array[0]
     song.name = filename_array[1]
     song
   end
 
   def self.create_from_filename(filename)
-    song = self.new
     filename_array = filename.chomp(".mp3").split(" - ")
+    song = self.create
     song.artist_name = filename_array[0]
     song.name = filename_array[1]
-    self.all << song
     song
   end
 
   def self.destroy_all
-    @@all = []
+    self.all.clear
   end
 
 end
